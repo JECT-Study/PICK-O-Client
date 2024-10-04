05d3e26495e341b6a9282da6799e0f4c267b50c6
onejuneee <onejuneee721@gmail.com>
2024-10-05 04:41:40 +0900

    typo: 스타일링 선언 방식을 템플릿 리터럴 방식으로 수정

--- src/components/organisms/SearchGameListSection/SearchGameListSection.style.ts
+++ src/components/organisms/SearchGameListSection/SearchGameListSection.style.ts
@@ -1,22 +1,23 @@
 import { css } from '@emotion/react';
 import typo from '@/styles/typo';
 
-export const containerStyle = css({
-  width: '1147px',
-});
+export const containerStyle = css`
+  width: 1147px;
+`;
 
-export const titleWrapStyle = css(typo.Title, {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom: '40px',
-});
+export const titleWrapStyle = css`
+  ${typo.Title};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+`;
 
-export const paginationWrapStyle = css({
-  display: 'flex',
-  justifyContent: 'center',
-  marginTop: '60px',
-  width: '100%',
-  height: '40px',
-  flexShrink: '0',
-});
+export const paginationWrapStyle = css`
+  display: flex;
+  justify-content: center;
+  margin-top: 60px;
+  width: 100%;
+  height: 40px;
+  flex-shrink: 0;
+`;
