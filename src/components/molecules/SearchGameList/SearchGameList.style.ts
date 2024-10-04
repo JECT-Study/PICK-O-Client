6e6440017f4e8097d33204542bd7f11c70ae93fe
onejuneee <onejuneee721@gmail.com>
2024-10-05 01:04:02 +0900

    fix: SearchGameList 구조 변경

--- src/components/molecules/SearchGameList/SearchGameList.style.ts
+++ src/components/molecules/SearchGameList/SearchGameList.style.ts
@@ -4,4 +4,7 @@ export const container = css`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(369px, 1fr));
   gap: 20px;
+  width: 100%;
+  max-width: 1147px;
+  margin: 0 auto;
 `;
