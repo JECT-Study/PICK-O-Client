bbcdbcd1232e1676508746171f469b071d686f33
onejuneee <onejuneee721@gmail.com>
2024-10-17 06:02:28 +0900

    refactor: SearchGameResult organisms 컴포넌트 구조 변경

--- src/components/organisms/SearchGameResult/SearchGameResult.tsx
+++ src/components/organisms/SearchGameResult/SearchGameResult.tsx
@@ -3,22 +3,32 @@ import MoreButton from '@/components/atoms/MoreButton/MoreButton';
 import SearchGameList, {
   GameItem,
 } from '@/components/molecules/SearchGameList/SearchGameList';
+import { NoResultsMessage } from '@/components/atoms/NoResultsMessage/NoResultsMessage';
 import * as S from './SearchGameResult.style';
 
 interface SearchGameResultProps {
   gameList: GameItem[];
+  keyword: string;
 }
 
-const SearchGameResult = ({ gameList }: SearchGameResultProps) => {
+const SearchGameResult = ({ gameList, keyword }: SearchGameResultProps) => {
+  const hasResults = gameList.length > 0;
+
   return (
     <div css={S.container}>
       <div css={S.titleWrapper}>
         <div>밸런스게임</div>
         <MoreButton />
       </div>
-      <div>
-        <SearchGameList gameList={gameList} />
-      </div>
+      {hasResults ? (
+        <div css={S.contentWRapper}>
+          <SearchGameList gameList={gameList} />
+        </div>
+      ) : (
+        <div css={S.noResultsWrapper}>
+          <NoResultsMessage searchChoice="default" keyword={keyword} />
+        </div>
+      )}
     </div>
   );
 };
