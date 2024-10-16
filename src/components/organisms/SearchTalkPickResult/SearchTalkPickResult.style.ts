e7d85342576f1d2a709ddb63f0936dd87ffedc04
onejuneee <onejuneee721@gmail.com>
2024-10-17 06:05:43 +0900

    refactor: SearchTalkPickResult organisms 컴포넌트 구조 변경

--- src/components/organisms/SearchTalkPickResult/SearchTalkPickResult.style.ts
+++ src/components/organisms/SearchTalkPickResult/SearchTalkPickResult.style.ts
@@ -1,14 +1,26 @@
 import { css } from '@emotion/react';
 import typo from '@/styles/typo';
+import color from '@/styles/color';
 
-export const container = css`
-  width: 1147px;
-`;
+export const container = css({
+  width: '1147px',
+  marginTop: '28px',
+  marginBottom: '60px',
+});
 
-export const titleWrapper = css`
-  ${typo.Title};
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 22px;
-`;
+export const titleWrapper = css(typo.Title, {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: '23px',
+});
+
+export const contentWrapper = css({
+  border: `1px solid ${color.GY[2]}`,
+  width: '1145px',
+  height: '224px',
+  borderRadius: '10px',
+  paddingTop: '49px',
+  paddingLeft: '34px',
+  background: color.WT,
+});
--- src/components/organisms/SearchTalkPickResult/SearchTalkPickResult.tsx
+++ src/components/organisms/SearchTalkPickResult/SearchTalkPickResult.tsx
@@ -2,24 +2,33 @@ import React from 'react';
 import MoreButton from '@/components/atoms/MoreButton/MoreButton';
 import { SearchTalkPickItemProps } from '@/components/atoms/SearchTalkPickItem/SearchTalkPickItem';
 import SearchTalkPickList from '@/components/molecules/SearchTalkPickList/SearchTalkPickList';
+import { NoResultsMessage } from '@/components/atoms/NoResultsMessage/NoResultsMessage';
 import * as S from './SearchTalkPickResult.style';
 
 interface SearchTalkPickResultProps {
   searchTalkPickList: SearchTalkPickItemProps[];
+  keyword: string;
 }
 
 const SearchTalkPickResult = ({
   searchTalkPickList,
+  keyword,
 }: SearchTalkPickResultProps) => {
+  const hasResults = searchTalkPickList.length > 0;
+
   return (
     <div css={S.container}>
       <div css={S.titleWrapper}>
         <div>톡픽</div>
         <MoreButton />
       </div>
-      <div>
+      {hasResults ? (
         <SearchTalkPickList searchTalkPickList={searchTalkPickList} />
-      </div>
+      ) : (
+        <div css={S.contentWrapper}>
+          <NoResultsMessage searchChoice="default" keyword={keyword} />
+        </div>
+      )}
     </div>
   );
 };
