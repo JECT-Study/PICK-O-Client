61c56fd4c08f4ae74cde39b52de491728fc1a533
onejuneee <onejuneee721@gmail.com>
2024-10-26 04:15:48 +0900

    feat: 밸런스게임 검색결과 페이지 구현

---
+++ src/pages/SearchResultsPage/SearchGamePage.tsx
@@ -0,0 +1,68 @@
+import * as S from '@/pages/SearchResultsPage/SearchResultsPage.style';
+import SearchResultBarContainer from '@/components/organisms/SearchResultBarContainer/SearchResultBarContainer';
+import Divider from '@/components/atoms/Divider/Divider';
+import React, { useState } from 'react';
+import SearchGameListSection from '@/components/organisms/SearchGameListSection/SearchGameListSection';
+import { useSearchParams } from 'react-router-dom';
+import { useGameResultQuery } from '@/hooks/api/search/useGameResultQuery';
+
+const SearchGamePage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+  const [selectedTag, setSelectedTag] = useState<'all' | 'talkpick' | 'game'>(
+    'game',
+  );
+  const [page, setPage] = useState(0);
+  const [sort, setSort] = useState('views');
+  const size = 9;
+
+  const {
+    content: gameResults,
+    totalPages: gameTotalPages,
+    isLoading,
+  } = useGameResultQuery(query, page, size, sort);
+
+  const handleTagClick = (value: 'all' | 'talkpick' | 'game') => {
+    setSelectedTag(value);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setPage(newPage - 1);
+    setSearchParams({ query, page: (newPage - 1).toString() });
+  };
+
+  const handleSortChange = (newSort: string) => {
+    setSort(newSort);
+    setPage(0);
+  };
+
+  if (isLoading) {
+    return <div>스켈레톤 준비중...</div>;
+  }
+
+  return (
+    <div css={S.container}>
+      <SearchResultBarContainer
+        selectedValue={selectedTag}
+        onClick={handleTagClick}
+        onSearch={(newQuery) => searchParams.set('query', newQuery)}
+      />
+      <div css={S.dividerWrapper}>
+        <Divider length={1133} orientation="width" />{' '}
+      </div>
+      <div css={S.resultsWrapper}>
+        <SearchGameListSection
+          gameList={gameResults}
+          keyword={query}
+          selectedPage={page + 1}
+          totalPages={gameTotalPages}
+          sort={sort}
+          onPageChange={handlePageChange}
+          onSortChange={handleSortChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchGamePage;
