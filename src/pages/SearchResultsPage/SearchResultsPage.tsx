68237841470f1c75c703e28372202eea585ca7d6
onejuneee <onejuneee721@gmail.com>
2024-10-26 04:09:01 +0900

    refactor: 검색결과 페이지를 전체 검색 결과 페이지로 세분화

--- src/pages/SearchResultsPage/SearchResultsPage.tsx
+++ src/pages/SearchResultsPage/SearchResultsPage.tsx
@@ -1,77 +1,35 @@
 import React, { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import SearchResultBar from '@/components/molecules/SearchResultBar/SearchResultBar';
 import SearchGameResult from '@/components/organisms/SearchGameResult/SearchGameResult';
 import SearchTalkPickResult from '@/components/organisms/SearchTalkPickResult/SearchTalkPickResult';
 import { NoResultsMessage } from '@/components/atoms/NoResultsMessage/NoResultsMessage';
-import SearchTalkPickListSection from '@/components/organisms/SearchTalkPickListSection/SearchTalkPickListSection';
-import SearchGameListSection from '@/components/organisms/SearchGameListSection/SearchGameListSection';
 import { useTalkPickResultQuery } from '@/hooks/api/search/useTalkPickResultQuery';
 import { useGameResultQuery } from '@/hooks/api/search/useGameResultQuery';
 import Divider from '@/components/atoms/Divider/Divider';
+import SearchResultBarContainer from '@/components/organisms/SearchResultBarContainer/SearchResultBarContainer';
+import { useSearchParams } from 'react-router-dom';
 import * as S from './SearchResultsPage.style';
 
 const SearchResultsPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const query = searchParams.get('query') || '';
-  const [selectedValue, setSelectedValue] = useState<
-    'all' | 'talkpick' | 'game'
-  >('all');
-  const [page, setPage] = useState(1);
-  const [sort, setSort] = useState('views');
+  const [selectedTag, setSelectedTag] = useState<'all' | 'talkpick' | 'game'>(
+    'all',
+  );
+  const page = 0;
+  const size = 4;
+  const sort = 'all';
 
-  const size = (() => {
-    switch (selectedValue) {
-      case 'all':
-        return 4;
-      case 'talkpick':
-        return 10;
-      case 'game':
-        return 9;
-      default:
-        return 4;
-    }
-  })();
-
-  const {
-    content: talkPickResults,
-    totalPages: talkPickTotalPages,
-    isLoading: isTalkPickLoading,
-  } = useTalkPickResultQuery(
+  const { content: talkPickResults, isLoading: isTalkPickLoading } =
+    useTalkPickResultQuery(query, page, size, sort);
+  const { content: gameResults, isLoading: isGameLoading } = useGameResultQuery(
     query,
     page,
     size,
     sort,
-    selectedValue === 'talkpick' || selectedValue === 'all',
   );
 
-  const {
-    content: gameResults,
-    totalPages: gameTotalPages,
-    isLoading: isGameLoading,
-  } = useGameResultQuery(
-    query,
-    page,
-    size,
-    sort,
-    selectedValue === 'game' || selectedValue === 'all',
-  );
-
-  const handleTabClick = (newTab: 'all' | 'talkpick' | 'game') => {
-    setSelectedValue(newTab);
-    setPage(1);
-  };
-
-  const handleSearch = (newQuery: string) => {
-    setSearchParams({ query: newQuery });
-  };
-
-  const handleSortChange = (newSort: string) => {
-    setSort(newSort);
-  };
-
-  const handlePageChange = (newPage: number) => {
-    setPage(newPage);
+  const handleTagClick = (value: 'all' | 'talkpick' | 'game') => {
+    setSelectedTag(value);
   };
 
   const renderResults = () => {
@@ -82,7 +40,7 @@ const SearchResultsPage = () => {
     const noTalkPickResults = talkPickResults.length === 0;
     const noGameResults = gameResults.length === 0;
 
-    if (selectedValue === 'all' && noTalkPickResults && noGameResults) {
+    if (selectedTag === 'all' && noTalkPickResults && noGameResults) {
       return (
         <div css={S.noResultsWrapper}>
           <NoResultsMessage searchChoice="default" keyword={query} />
@@ -90,52 +48,23 @@ const SearchResultsPage = () => {
       );
     }
 
-    switch (selectedValue) {
-      case 'all':
-        return (
-          <>
-            <SearchTalkPickResult
-              searchTalkPickList={talkPickResults}
-              keyword={query}
-            />
-            <SearchGameResult gameList={gameResults} keyword={query} />
-          </>
-        );
-      case 'talkpick':
-        return (
-          <SearchTalkPickListSection
-            searchTalkPickList={talkPickResults}
-            keyword={query}
-            selectedPage={page}
-            totalPages={talkPickTotalPages}
-            onPageChange={handlePageChange}
-            sort={sort}
-            onSortChange={handleSortChange}
-          />
-        );
-      case 'game':
-        return (
-          <SearchGameListSection
-            gameList={gameResults}
-            keyword={query}
-            selectedPage={page}
-            totalPages={gameTotalPages}
-            sort={sort}
-            onPageChange={handlePageChange}
-            onSortChange={handleSortChange}
-          />
-        );
-      default:
-        return null;
-    }
+    return (
+      <div css={S.resultsWrapper}>
+        <SearchTalkPickResult
+          searchTalkPickList={talkPickResults}
+          keyword={query}
+        />
+        <SearchGameResult gameList={gameResults} keyword={query} />
+      </div>
+    );
   };
 
   return (
     <div css={S.container}>
-      <SearchResultBar
-        selectedValue={selectedValue}
-        onClick={handleTabClick}
-        onSearch={handleSearch}
+      <SearchResultBarContainer
+        selectedValue={selectedTag}
+        onClick={handleTagClick}
+        onSearch={(newQuery) => searchParams.set('query', newQuery)}
       />
       <div css={S.dividerWrapper}>
         <Divider length={1133} orientation="width" />{' '}
