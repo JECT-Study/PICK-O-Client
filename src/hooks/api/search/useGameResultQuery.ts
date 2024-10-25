77ff280f65d5fdf3deadf06cb4403439c4cfed31
onejuneee <onejuneee721@gmail.com>
2024-10-26 03:45:41 +0900

    refactor: 커스텀 훅이 content, totalpages, isloading만 반환하도록 변경

--- src/hooks/api/search/useGameResultQuery.ts
+++ src/hooks/api/search/useGameResultQuery.ts
@@ -7,21 +7,14 @@ export const useGameResultQuery = (
   page: number,
   size: number,
   sort: string,
-  enabledCondition: boolean,
 ) => {
-  const {
-    data: gameResults,
-    isLoading,
-    isError,
-  } = useQuery<GameResult>({
+  const { data: gameResults, isLoading } = useQuery<GameResult>({
     queryKey: ['gameResults', query, page, size, sort],
     queryFn: () => getGameResults(query, page, size, sort),
-    enabled: enabledCondition,
   });
 
   const content = gameResults?.content || [];
   const totalPages = gameResults?.totalPages || 0;
-  console.log('게임 쿼리 Query 상태:', { content });
 
-  return { content, totalPages, isLoading, isError };
+  return { content, totalPages, isLoading };
 };
--- src/hooks/api/search/useTalkPickResultQuery.ts
+++ src/hooks/api/search/useTalkPickResultQuery.ts
@@ -7,21 +7,14 @@ export const useTalkPickResultQuery = (
   page: number,
   size: number,
   sort: string,
-  enabledCondition: boolean,
 ) => {
-  const {
-    data: talkPickResults,
-    isLoading,
-    isError,
-  } = useQuery<TalkPickResult>({
+  const { data: talkPickResults, isLoading } = useQuery<TalkPickResult>({
     queryKey: ['talkPickResults', query, page, size, sort],
     queryFn: () => getTalkPickResults(query, page, size, sort),
-    enabled: enabledCondition,
   });
 
   const content = talkPickResults?.content || [];
   const totalPages = talkPickResults?.totalPages || 0;
-  console.log('톡픽 쿼리 실행 Query :', { content });
 
-  return { content, totalPages, isLoading, isError };
+  return { content, totalPages, isLoading };
 };
