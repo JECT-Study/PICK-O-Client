0e06defc86f689dfcb10e0c592db04e893d8049f
onejuneee <onejuneee721@gmail.com>
2024-10-26 21:56:54 +0900

    refactor: SearchTalkPickResult 스토리북 브라우저 라우터 추가

--- src/stories/organisms/SearchTalkPickResult.stories.tsx
+++ src/stories/organisms/SearchTalkPickResult.stories.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import SearchTalkPickResult from '@/components/organisms/SearchTalkPickResult/SearchTalkPickResult';
+import { BrowserRouter } from 'react-router-dom';
 import { SampleWhole } from '@/assets';
-import { SearchTalkPickItemProps } from '@/components/atoms/SearchTalkPickItem/SearchTalkPickItem';
+import type { SearchTalkPickItemProps } from '@/components/atoms/SearchTalkPickItem/SearchTalkPickItem';
 
 const SearchTalkPickItems: SearchTalkPickItemProps[] = Array.from(
   { length: 4 },
   (_, index) => ({
     title: `톡픽 ${index + 1} - 월클 정국 VS 존잘 차은우`,
-    date: '2024.08.26',
+    createdAt: '2024.08.26',
     content: '우하하우하하 내용입니다.',
-    imgUrl: SampleWhole,
+    firstImgUrl: SampleWhole,
+    keyword: '정국',
   }),
 );
 
-const meta = {
+const meta: Meta<typeof SearchTalkPickResult> = {
   title: 'organisms/SearchTalkPickResult',
   component: SearchTalkPickResult,
   tags: ['autodocs'],
   args: {
     searchTalkPickList: SearchTalkPickItems,
+    keyword: '정국',
   },
-} satisfies Meta<typeof SearchTalkPickResult>;
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
