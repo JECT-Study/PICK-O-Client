bf056833d74b5349bf71ff08d9abdd4ef03b9394
onejuneee <onejuneee721@gmail.com>
2024-10-26 21:52:17 +0900

    refactor: 브라우저 라우터 선언 위치 변경

--- src/stories/organisms/SearchGameResult.stories.tsx
+++ src/stories/organisms/SearchGameResult.stories.tsx
@@ -1,4 +1,5 @@
-import { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 import SearchGameResult from '@/components/organisms/SearchGameResult/SearchGameResult';
 import { BrowserRouter } from 'react-router-dom';
 import { SampleFirst, SampleSecond } from '@/assets';
@@ -9,24 +10,6 @@ const meta: Meta<typeof SearchGameResult> = {
   parameters: {
     layout: 'centered',
   },
-};
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-const gameListSample = Array.from({ length: 9 }, (_, index) => ({
-  optionAImg: SampleFirst,
-  optionBImg: SampleSecond,
-  title: `게임 ${index + 1} - 유진 VS 민지 사복 고르기`,
-  mainTag: '취향',
-  subTag: '얼마나 맞나 보자',
-}));
-
-export const Default: Story = {
-  args: {
-    gameList: gameListSample,
-    keyword: '유진',
-  },
   decorators: [
     (Story) => (
       <BrowserRouter>
@@ -36,33 +19,42 @@ export const Default: Story = {
   ],
 };
 
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const createGameList = (length: number) =>
+  Array.from({ length }, (_, index) => ({
+    optionAImg: SampleFirst,
+    optionBImg: SampleSecond,
+    title: `게임 ${index + 1} - 유진 VS 민지 사복 고르기`,
+    mainTag: '취향',
+    subTag: '얼마나 맞나 보자',
+  }));
+
+const gameListSample = createGameList(9);
+
+export const Default: Story = {
+  args: {
+    gameList: gameListSample,
+    keyword: '유진',
+  },
+};
+
 export const All: Story = {
   render: (args) => (
-    <BrowserRouter>
-      <>
-        <div style={{ marginBottom: '40px' }}>
-          <h3>게임 목록이 9개 미만인 경우 (5개)</h3>
-          <SearchGameResult
-            {...args}
-            gameList={Array.from({ length: 5 }, (_, index) => ({
-              optionAImg: SampleFirst,
-              optionBImg: SampleSecond,
-              title: `게임 ${index + 1} - 유진 VS 민지 사복 고르기`,
-              mainTag: '취향',
-              subTag: '얼마나 맞나 보자',
-            }))}
-            keyword="유진"
-          />
-        </div>
-        <div>
-          <h3>게임 목록이 9개인 경우</h3>
-          <SearchGameResult
-            {...args}
-            gameList={gameListSample}
-            keyword="유진"
-          />
-        </div>
-      </>
-    </BrowserRouter>
+    <>
+      <div style={{ marginBottom: '40px' }}>
+        <h3>게임 목록이 9개 미만인 경우 (5개)</h3>
+        <SearchGameResult
+          {...args}
+          gameList={createGameList(5)}
+          keyword="유진"
+        />
+      </div>
+      <div>
+        <h3>게임 목록이 9개인 경우</h3>
+        <SearchGameResult {...args} gameList={gameListSample} keyword="유진" />
+      </div>
+    </>
   ),
 };
