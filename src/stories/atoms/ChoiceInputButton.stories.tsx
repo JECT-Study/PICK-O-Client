f1360031653a0d2764e52d24b864261971c6aaca
onejuneee <onejuneee721@gmail.com>
2024-11-06 01:19:57 +0900

    refactor: 수정된 ChoiceInputButton 스토리북 반영

--- src/stories/atoms/ChoiceInputButton.stories.tsx
+++ src/stories/atoms/ChoiceInputButton.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import ChoiceInputButton from '@/components/atoms/ChoiceInputButton/ChoiceInputButton';
 import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
@@ -9,34 +9,64 @@ const meta: Meta<typeof ChoiceInputButton> = {
   parameters: {
     layout: 'centered',
   },
-  tags: ['autodocs'],
   argTypes: {
     option: {
       options: ['A', 'B'],
       control: { type: 'radio' },
     },
-  },
-  args: {
-    option: 'A',
+    resetInfoInput: {
+      control: 'boolean',
+    },
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args) => <ChoiceInputButton {...args} />,
+};
 
 export const All: Story = {
-  render: () => (
-    <ul css={storyContainer}>
-      <li css={storyInnerContainer}>
-        <h3>A ChoiceInputButton</h3>
-        <ChoiceInputButton option="A" />
-      </li>
-      <li css={storyInnerContainer}>
-        <h3>B ChoiceInputButton</h3>
-        <ChoiceInputButton option="B" />
-      </li>
-    </ul>
-  ),
+  render: () => {
+    const [resetInfoA, setResetInfoA] = useState(false);
+    const [resetInfoB, setResetInfoB] = useState(false);
+
+    return (
+      <ul css={storyContainer}>
+        <li css={storyInnerContainer}>
+          <h3>A 선택지</h3>
+          <ChoiceInputButton
+            option="A"
+            resetInfoInput={resetInfoA}
+            choiceInputProps={{
+              placeholder: 'A 선택지를 입력하세요.',
+              value: '',
+              onChange: () => setResetInfoA(false),
+            }}
+            infoInputProps={{
+              placeholder: '해당 선택지에 대해 추가로 설명을 입력할 수 있어요!',
+              value: '',
+            }}
+          />
+        </li>
+        <li css={storyInnerContainer}>
+          <h3>B 선택지</h3>
+          <ChoiceInputButton
+            option="B"
+            resetInfoInput={resetInfoB}
+            choiceInputProps={{
+              placeholder: 'B 선택지를 입력하세요.',
+              value: '',
+              onChange: () => setResetInfoB(false),
+            }}
+            infoInputProps={{
+              placeholder: '해당 선택지에 대해 추가로 설명을 입력할 수 있어요!',
+              value: '',
+            }}
+          />
+        </li>
+      </ul>
+    );
+  },
 };
