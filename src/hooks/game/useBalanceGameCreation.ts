15d5fcb99240485962fcd2339ea69f9c65382271
onejuneee <onejuneee721@gmail.com>
2024-11-06 03:03:33 +0900

    feat: BalanceGameCreation 을 관리하는 커스텀 훅 구현

---
+++ src/hooks/game/useBalanceGameCreation.ts
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+import { BalanceGameOption, BalanceGameSet } from '@/types/game';
+import { createImageUrlFromFile } from '@/utils/file';
+
+const initializeGames = (totalStage: number): BalanceGameSet[] =>
+  Array.from({ length: totalStage }, (_, idx) => ({
+    description: '',
+    gameOptions: [
+      {
+        id: idx * 2,
+        name: '',
+        imgUrl: '',
+        storedName: '',
+        description: '',
+        optionType: 'A',
+      },
+      {
+        id: idx * 2 + 1,
+        name: '',
+        imgUrl: '',
+        storedName: '',
+        description: '',
+        optionType: 'B',
+      },
+    ],
+  }));
+
+export const useBalanceGameCreation = (totalStage: number) => {
+  const [games, setGames] = useState<BalanceGameSet[]>(
+    initializeGames(totalStage),
+  );
+  const [currentStage, setCurrentStage] = useState(0);
+  const [currentOptions, setCurrentOptions] = useState<BalanceGameOption[]>(
+    games[0].gameOptions,
+  );
+  const [resetInfoInput, setResetInfoInput] = useState(false);
+
+  useEffect(() => {
+    if (resetInfoInput) {
+      setResetInfoInput(false);
+    }
+  }, [resetInfoInput]);
+
+  useEffect(() => {
+    setCurrentOptions(games[currentStage].gameOptions);
+  }, [currentStage, games]);
+
+  const updateOption = (
+    stageIndex: number,
+    optionType: 'A' | 'B',
+    newOption: Partial<BalanceGameOption>,
+  ) => {
+    setGames((prevGames) =>
+      prevGames.map((game, idx) =>
+        idx === stageIndex
+          ? {
+              ...game,
+              gameOptions: game.gameOptions.map((opt) =>
+                opt.optionType === optionType ? { ...opt, ...newOption } : opt,
+              ),
+            }
+          : game,
+      ),
+    );
+  };
+
+  const handleImageAChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imgUrl = createImageUrlFromFile(file);
+      updateOption(currentStage, 'A', {
+        imgUrl,
+        storedName: file.name,
+        imageFile: file,
+      });
+    }
+  };
+
+  const handleImageBChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imgUrl = createImageUrlFromFile(file);
+      updateOption(currentStage, 'B', {
+        imgUrl,
+        storedName: file.name,
+        imageFile: file,
+      });
+    }
+  };
+
+  const handleOptionAChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateOption(currentStage, 'A', { name: event.target.value });
+  };
+
+  const handleOptionBChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateOption(currentStage, 'B', { name: event.target.value });
+  };
+
+  const handleDescriptionChange = (
+    optionType: 'A' | 'B',
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const { value } = event.target;
+    updateOption(currentStage, optionType, { description: value });
+  };
+
+  const handleNextStage = () => {
+    if (currentStage < totalStage - 1) {
+      setResetInfoInput(true);
+      setCurrentStage((prev) => prev + 1);
+    }
+  };
+
+  const handlePrevStage = () => {
+    if (currentStage > 0) {
+      setResetInfoInput(true);
+      setCurrentStage((prev) => prev - 1);
+    }
+  };
+
+  return {
+    currentStage,
+    games,
+    currentOptions,
+    resetInfoInput,
+    handleImageAChange,
+    handleImageBChange,
+    handleOptionAChange,
+    handleOptionBChange,
+    handleNextStage,
+    handlePrevStage,
+    handleDescriptionChange,
+  };
+};
