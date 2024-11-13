c2b879b2d292d1f4487b48710d2e5d17f34ee582
areumH <94692566+areumH@users.noreply.github.com>
2024-11-14 03:47:25 +0900

    refactor: 불필요한 주석 제거

--- src/api/email.ts
+++ src/api/email.ts
@@ -3,11 +3,6 @@ import { ServerResponse } from '@/types/api';
 import { MemberResetForm, MemberVerifyForm } from '@/types/member';
 import { axiosInstance } from './interceptor';
 
-// EMAIL_SIGNUP_CODE: '/email/signup/code', 회원가입 때 발송 (이메일)
-// EMAIL_RESET_CODE: '/email/reset/code', 재설정 때 발송 (이메일)
-// EMAIL_VERIFY: '/email/verify', 인증번호 검증 (이메일, 인증번호)
-// EMAIL_RESET: '/email/reset', 비번 초기화 (이메일, 비번, 비번확인)
-
 export const postSignUpCode = async (email: string) => {
   const { data } = await axiosInstance.post<ServerResponse>(
     `${END_POINT.EMAIL_SIGNUP_CODE}`,
