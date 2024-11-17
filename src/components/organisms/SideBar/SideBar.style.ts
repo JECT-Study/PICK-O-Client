50b7ee49ebcae022eff986996a2fa9dc2adf1c3b
onejuneee <onejuneee721@gmail.com>
2024-11-18 04:05:32 +0900

    refactor: isLoading 유무에 따른 스타일링 단순화

--- src/components/organisms/SideBar/SideBar.style.ts
+++ src/components/organisms/SideBar/SideBar.style.ts
@@ -1,20 +1,28 @@
 import { css } from '@emotion/react';
 import color from '@/styles/color';
 
+const commonSideBarContainer = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  borderRadius: '20px',
+  width: '258px',
+  height: '538px',
+  flexShrink: 0,
+  paddingTop: '30px',
+  paddingBottom: '35px',
+});
+
 export const sidebarContainer = (isLoading: boolean) =>
-  css({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: isLoading ? color.GY[3] : color.WT,
-    borderRadius: '20px',
-    width: '258px',
-    height: '538px',
-    flexShrink: 0,
-    border: isLoading ? '' : `1px solid ${color.SKYBLUE}`,
-    paddingTop: '30px',
-    paddingBottom: '35px',
-  });
+  css([
+    commonSideBarContainer,
+    isLoading
+      ? { backgroundColor: color.GY[3] }
+      : {
+          backgroundColor: color.WT,
+          border: `1px solid ${color.SKYBLUE}`,
+        },
+  ]);
 
 export const profileWrapper = css({
   display: 'flex',
