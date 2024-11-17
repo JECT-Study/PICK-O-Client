b944216cb8e44814579321a2e64c374429b39ae3
onejuneee <onejuneee721@gmail.com>
2024-11-18 03:59:10 +0900

    refactor: Sidebar organisms 컴포넌트의 인터페이스가 특정 컴포넌트에 귀속되지 않도록 수정

--- src/components/organisms/SideBar/SideBar.tsx
+++ src/components/organisms/SideBar/SideBar.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import ProfileIcon, {
-  ProfileProps,
-} from '@/components/atoms/ProfileIcon/ProfileIcon';
-import ProfileLabel, {
-  ProfileLabelProps,
-} from '@/components/atoms/ProfileLabel/ProfileLabel';
-import SideBox, { SideBoxProps } from '@/components/molecules/SideBox/SideBox';
+import ProfileIcon from '@/components/atoms/ProfileIcon/ProfileIcon';
+import ProfileLabel from '@/components/atoms/ProfileLabel/ProfileLabel';
+import SideBox from '@/components/molecules/SideBox/SideBox';
 import ActionBox from '@/components/molecules/ActionBox/ActionBox';
 import * as S from './SideBar.style';
 
 export interface SideBarProps {
-  nickname?: ProfileLabelProps['nickname'];
-  postsCount?: SideBoxProps['postsCount'];
-  bookmarkedPostsCount?: SideBoxProps['bookmarkedPostsCount'];
-  profileImageUrl?: ProfileProps['imgUrl'];
+  nickname: string;
+  postsCount: number;
+  bookmarkedPostsCount: number;
+  profileImageUrl?: string;
   isLoading?: boolean;
 }
 
