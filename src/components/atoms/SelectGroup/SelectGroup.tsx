a80f3febfdab4696b20dbeb68a71ab12b414911a
onejuneee <onejuneee721@gmail.com>
2024-11-18 08:15:04 +0900

    refactor: selectGroup이 항상 두개의 아이템 배열을 받도록 수정

--- src/components/atoms/SelectGroup/SelectGroup.tsx
+++ src/components/atoms/SelectGroup/SelectGroup.tsx
@@ -7,28 +7,26 @@ export type SelectGroupItem = {
 };
 
 export interface SelectGroupProps {
-  items: SelectGroupItem[];
+  items: [SelectGroupItem, SelectGroupItem];
   selectedValue?: string;
   onSelect?: (value: string) => void;
 }
 const SelectGroup = ({ items, selectedValue, onSelect }: SelectGroupProps) => (
   <div css={S.selectGroupStyling}>
-    {Array.isArray(items) &&
-      items.length === 2 &&
-      items.map((item) => (
-        <button
-          key={item.value}
-          type="button"
-          css={[
-            S.selectGroupItemStyling,
-            S.itemRadius,
-            item.value === selectedValue && S.selectedStyling,
-          ]}
-          onClick={() => onSelect?.(item.value)}
-        >
-          {item.label}
-        </button>
-      ))}
+    {items.map((item) => (
+      <button
+        key={item.value}
+        type="button"
+        css={[
+          S.selectGroupItemStyling,
+          S.itemRadius,
+          item.value === selectedValue && S.selectedStyling,
+        ]}
+        onClick={() => onSelect?.(item.value)}
+      >
+        {item.label}
+      </button>
+    ))}
     <div css={S.bottomBarStyling} />
   </div>
 );
