365469a93cddfa41a4d489aaffc88d55d4e17119
onejuneee <onejuneee721@gmail.com>
2024-11-18 17:16:58 +0900

    refactor: 내 정보를 불러올 때 필요한 정보만 호출하도록 수정

--- src/hooks/api/mypages/useMyInfoQuery.ts
+++ src/hooks/api/mypages/useMyInfoQuery.ts
@@ -4,11 +4,20 @@ import { SideBar } from '@/types/mypages';
 import { Id } from '@/types/api';
 
 export const useMyInfoQuery = (memberId: Id) => {
-  const { data: memberInfo, isLoading } = useQuery<SideBar>({
+  const { data, isLoading } = useQuery<SideBar>({
     queryKey: ['memberInfo', memberId],
     queryFn: () => getMyInfo(memberId),
     enabled: !!memberId,
   });
 
-  return { memberInfo, isLoading };
+  const formattedMemberInfo: SideBar | null = data
+    ? {
+        ...data,
+        profileImageUrl: data.profileImageUrl || '',
+        postsCount: data.postsCount || 0,
+        bookmarkedPostsCount: data.bookmarkedPostsCount || 0,
+      }
+    : null;
+
+  return { memberInfo: formattedMemberInfo, isLoading };
 };
