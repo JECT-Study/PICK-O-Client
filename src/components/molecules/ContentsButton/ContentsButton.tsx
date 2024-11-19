060ec251fdd407399c372734a5bcff4735fcd3fb
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-11-19 15:42:55 +0900

    fix: SearchGameList props 에러 해결

--- src/components/molecules/ContentsButton/ContentsButton.tsx
+++ src/components/molecules/ContentsButton/ContentsButton.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React, { ComponentPropsWithRef } from 'react';
 import Chips from '@/components/atoms/Chips/Chips';
 import Bookmark, { BookmarkProps } from '@/components/atoms/Bookmark/Bookmark';
