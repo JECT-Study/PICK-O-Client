84d1233b95b7c4618e511b77cea3096c747ce461
areumH <94692566+areumH@users.noreply.github.com>
2024-11-25 15:42:38 +0900

    refactor: CitationBox 컴포넌트 props 처리 개선

--- src/components/atoms/CitationBox/CitationBox.tsx
+++ src/components/atoms/CitationBox/CitationBox.tsx
@@ -1,9 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { forwardRef } from 'react';
-import type { ForwardedRef } from 'react';
+import type { ComponentPropsWithRef, ForwardedRef } from 'react';
 import * as S from './CitationBox.style';
 
-const CitationBox = (props: any, ref: ForwardedRef<HTMLInputElement>) => {
+export interface CitationBoxProps extends ComponentPropsWithRef<'input'> {}
+
+const CitationBox = (
+  props: CitationBoxProps,
+  ref: ForwardedRef<HTMLInputElement>,
+) => {
   return (
     <div css={S.boxStyle}>
       <div css={S.boxTitleStyle}>출처</div>
