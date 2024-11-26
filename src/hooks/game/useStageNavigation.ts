013b791ddf157d0d071d476d7229b40211511f25
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-11-26 15:16:41 +0900

    refactor: 밸런스 게임 제작 이미지 업로드 로직 수정 (#235)
    * move: 사용하지 않는 import 문 제거
    
    * feat: 수정된 이미지 업로드 api에 맞춰 인터페이스 변경
    
    * feat: 수정된 이미지 업로드 api에 맞춰 인터페이스 변경
    
    * refactor: 이미지 업로드를 단일한 건으로 수행하도록 수정
    
    * refactor: 이미지 파일 처리를 상위 페이지에서 하도록 수정
    
    * refactor: 이미지 파일 처리를 상위 페이지에서 하도록 수정
    
    * refactor: 이미지 파일 처리를 상위 페이지에서 하도록 수정
    
    * refactor: 임시 저장 인터페이스를 수정된 api에 맞게 구현
    
    * refactor: 이미지 업로드를 api를 통해서만 처리하도록 수정
    
    * refactor: 이미지 업로드를 상위 페이지에서 처리할 수 있도록 props 수정
    
    * refactor: 기본 게임 세트 생성 시 수정된 인터페이스로 생성하도록 수정
    
    * refactor: 밸런스 게임 세트 생성 시 설명란도 각 세트마다 할당되도록 설정
    
    * refactor: 밸런스 게임 생성 시 이미지 처리와 설명 처리 관심사 분리
    
    * refactor: 밸런스 게임 세트 별 description 처리를 페이지에서 하도록 수정
    
    * refactor: 스테이징 처리 시 자동으로 clearInput을 초기화 하도록 수정
    
    * move: 불필요한 디버깅용 코드 제거
    
    * refactor: 밸런스게임 생성 시 반환값도 함께 감지하도록 설정
    
    * feat: 삭제 처리를 위한 아이콘 추가
    
    * feat: 이미지 삭제 로직을 위한 콜백 함수를 props로 처리
    
    * feat: 상위 컴포넌트로 콜백 props 전달
    
    * feat: 페이지에서 이미지 삭제 로직 추가
    
    * feat: 게임 생성 완료 후 네비게이션 로직 추가
    
    * feat: 임시 저장, 불러오기 로직을 임시 비활성화
    
    * refactor: 토스트 모달의 메시지를 훅을 이용해 일괄 관리

--- src/hooks/game/useStageNavigation.ts
+++ src/hooks/game/useStageNavigation.ts
@@ -1,8 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useStageNavigation = (totalStage: number) => {
   const [currentStage, setCurrentStage] = useState(0);
   const [clearInput, setClearInput] = useState(false);
+  const timerRef = useRef<number | NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (clearInput) {
+      if (timerRef.current !== null) {
+        clearTimeout(timerRef.current);
+      }
+
+      timerRef.current = setTimeout(() => {
+        setClearInput(false);
+      }, 500);
+
+      return () => {
+        if (timerRef.current !== null) {
+          clearTimeout(timerRef.current);
+        }
+      };
+    }
+  }, [currentStage, clearInput]);
 
   const handleNextStage = () => {
     if (currentStage < totalStage - 1) {
