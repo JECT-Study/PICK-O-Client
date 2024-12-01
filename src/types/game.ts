e952f67f43867f52223e4638427f5533750a6e1a
onejuneee <onejuneee721@gmail.com>
2024-12-02 01:11:24 +0900

    refactor: 임시 저장 인터페이스 수정 및 사용하지 않는 인터페이스 임시 주석 처리

--- src/types/game.ts
+++ src/types/game.ts
@@ -71,38 +71,58 @@ export interface BalanceGame {
   subTag: string;
   games: BalanceGameSet[];
 }
+//
+// export interface TempGame {
+//   mainTag: string;
+//   subTag: string;
+//   tempGames: TempGameSet[];
+// }
+//
+// export interface TempGameSet {
+//   title: string;
+//   description: string;
+//   tempGameOptions: {
+//     name: string;
+//     imgUrl: string;
+//     fileId: number;
+//     description: string;
+//     optionType: 'A' | 'B';
+//   }[];
+// }
+//
+// export interface TempGameOptionResponse {
+//   name: string;
+//   description: string;
+//   imgUrl: string;
+//   optionType: 'A' | 'B';
+// }
+//
+// export interface TempGameDetailResponse {
+//   title: string;
+//   mainTag: string;
+//   subTag: string;
+//   tempGames: TempGameOptionResponse[];
+// }
+//
+// export interface TempGameResponse {
+//   tempGameDetailResponses: TempGameDetailResponse[];
+// }
 
-export interface TempGame {
-  mainTag: string;
-  subTag: string;
-  tempGames: TempGameSet[];
-}
-
-export interface TempGameSet {
-  title: string;
-  description: string;
-  tempGameOptions: {
-    name: string;
-    imgUrl: string;
-    fileId: number;
-    description: string;
-    optionType: 'A' | 'B';
-  }[];
-}
-
-export interface TempGameOptionResponse {
+export interface TempGameOption {
   name: string;
   description: string;
-  imgUrl: string;
+  fileId?: number | null;
   optionType: 'A' | 'B';
 }
 
-export interface TempGameDetailResponse {
-  title: string;
+export interface TempGameSet {
   description: string;
-  tempGameOptions: TempGameOptionResponse[];
+  tempGameOptions: TempGameOption[];
 }
 
-export interface TempGameResponse {
-  tempGameDetailResponses: TempGameDetailResponse[];
+export interface TempGame {
+  title: string;
+  mainTag: string;
+  subTag: string;
+  tempGames: TempGameSet[];
 }
