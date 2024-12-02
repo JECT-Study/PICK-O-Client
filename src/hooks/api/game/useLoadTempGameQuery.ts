7f70c02cda41360750930de6af92c1a5bbda5007
onejuneee <onejuneee721@gmail.com>
2024-12-02 15:33:00 +0900

    fix: 임시 저장 불러오기 훅의 이름을 조금 더 구체적으로 설정

--- src/hooks/api/game/useLoadTempGameQuery.ts
+++ src/hooks/api/game/useLoadTempGameQuery.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { TempGame } from '@/types/game';
 import { getTempGame } from '@/api/game';
-import { TempGameResponse } from '@/types/game';
 
-export const useTempGameQuery = () => {
-  return useQuery<TempGameResponse>({
+export const useLoadTempGameQuery = () => {
+  return useQuery<TempGame>({
     queryKey: ['tempGame'],
     queryFn: getTempGame,
-    enabled: false,
+    staleTime: 5 * 60 * 1000,
   });
 };
