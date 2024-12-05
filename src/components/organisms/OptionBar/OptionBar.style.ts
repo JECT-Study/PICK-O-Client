b3978af953c9dc741cb2cf0c2fc8924a7817679f
onejuneee <onejuneee721@gmail.com>
2024-12-06 05:11:08 +0900

    refactor: 부모 요소의 width를 자식 width에서 오버플로우 되지 않게 수정

--- src/components/organisms/OptionBar/OptionBar.style.ts
+++ src/components/organisms/OptionBar/OptionBar.style.ts
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 export const container = css({
   display: 'flex',
   flexDirection: 'column',
-  width: '904px',
+  width: '912px',
   alignItems: 'center',
   gap: '20px',
 });
