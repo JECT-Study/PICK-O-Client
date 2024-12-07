5a43a063ff9d54160c7658dd2da904fecc106821
areumH <94692566+areumH@users.noreply.github.com>
2024-12-08 03:05:00 +0900

    design: 모바일 GameStageLabel 컴포넌트 구현 및 스토리북 작성

---
+++ src/stories/mobile/atoms/GameStageLabel.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import GameStageLabel from '@/components/mobile/atoms/GameStageLabel/GameStageLabel';
+import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
+
+const meta = {
+  title: 'mobile/atoms/GameStageLabel',
+  component: GameStageLabel,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    color: {
+      options: ['main', 'default'],
+      control: { type: 'radio' },
+    },
+    stage: {
+      control: { type: 'range', min: 0, max: 9 },
+    },
+  },
+  args: {
+    stage: 3,
+    color: 'default',
+  },
+} satisfies Meta<typeof GameStageLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const All: Story = {
+  render: (args) => (
+    <ul css={storyContainer}>
+      <li css={storyInnerContainer}>
+        <h1>default</h1>
+        <GameStageLabel {...args} color="default" />
+        <h1>main</h1>
+        <GameStageLabel {...args} color="main" />
+      </li>
+    </ul>
+  ),
+};
