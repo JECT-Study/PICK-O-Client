42ed788b49b1018cc40c49e22b1bb3c57986a164
areumH <94692566+areumH@users.noreply.github.com>
2024-12-08 03:10:41 +0900

    design: 모바일 GameTag 컴포넌트 구현 및 스토리북 작성

---
+++ src/components/mobile/atoms/GameTag/GameTag.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './GameTag.style';
+
+export interface GameTagProps {
+  tag?: string;
+}
+
+const GameTag = ({ tag }: GameTagProps) => (
+  <div css={S.tagStyle}>
+    <span>#</span> <span>{tag}</span>
+  </div>
+);
+
+export default GameTag;
---
+++ src/stories/mobile/atoms/GameTag.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import GameTag from '@/components/mobile/atoms/GameTag/GameTag';
+
+const meta = {
+  title: 'mobile/atoms/GameTag',
+  component: GameTag,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    tag: { control: { type: 'text' } },
+  },
+  args: {
+    tag: '커플',
+  },
+} satisfies Meta<typeof GameTag>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
