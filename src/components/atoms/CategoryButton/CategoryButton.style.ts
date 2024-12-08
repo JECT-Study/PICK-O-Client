2e3426779f4360aa2f6bd50235b589a9118df664
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-12-08 18:27:08 +0900

    [feat] : CategoryBox 관련 모바일 스타일 추가

--- src/components/atoms/CategoryButton/CategoryButton.style.ts
+++ src/components/atoms/CategoryButton/CategoryButton.style.ts
@@ -2,27 +2,37 @@ import { css } from '@emotion/react';
 import color from '@/styles/color';
 import typo from '@/styles/typo';
 
-export const categoryButtonBaseStyle = css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 367px;
-  height: 189px;
-  padding: 40px 85px;
-  border-radius: 20px;
-  border: 1px solid #dedede;
-  background-color: ${color.WT};
-  box-shadow: 1px 1px 15px 0 rgba(0, 0, 0, 0.1);
-  cursor: pointer;
-  transition: all 0.3s ease-in;
-`;
+export const categoryButtonBaseStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '367px',
+  height: '189px',
+  padding: '40px 85px',
+  borderRadius: '20px',
+  border: '1px solid #dedede',
+  backgroundColor: color.WT,
+  boxShadow: '1px 1px 15px 0 rgba(0, 0, 0, 0.1)',
+  cursor: 'pointer',
+  transition: 'all 0.3s ease-in',
+  '@media (max-width: 430px)': {
+    width: '164px',
+    height: '70px',
+    padding: '5px 50px 14px',
+    borderRadius: '7px',
+    border: '0.35px solid #dedede',
+  },
+});
 
-export const imgWrap = css`
-  flex-shrink: 0;
-`;
+export const imgWrap = css({
+  flexShrink: 0,
+});
 
-export const labelStyle = css`
-  ${typo.SubTitle}
-  margin-top: auto;
-`;
+export const labelStyle = css({
+  ...typo.SubTitle,
+  marginTop: 'auto',
+  '@media (max-width: 430px)': {
+    ...typo.Mobile.Text.Medium_8,
+  },
+});
--- src/components/molecules/CategoryBox/CategoryBox.style.ts
+++ src/components/molecules/CategoryBox/CategoryBox.style.ts
@@ -2,5 +2,11 @@ import { css } from '@emotion/react';
 
 export const categoryBoxStyling = css({
   display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   gap: '20px',
+  width: '100%',
+  '@media (max-width: 430px)': {
+    gap: '7px',
+  },
 });
