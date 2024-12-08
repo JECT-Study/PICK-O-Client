33de30d9b09bc5d843b10236880afc87a66f5c05
onejuneee <onejuneee721@gmail.com>
2024-12-09 05:39:20 +0900

    feat: 마이페이지 토글 그룹 스토리북 추가

---
+++ src/components/mobile/atoms/SelectGroup.stories.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
+import SelectGroup from '@/components/mobile/atoms/SelectGroup';
+
+const meta = {
+  title: 'mobile/atoms/SelectGroup',
+  component: SelectGroup,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    selectedValue: { control: 'text' },
+    onSelect: { action: 'selected' },
+  },
+} satisfies Meta<typeof SelectGroup>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      { label: '톡픽', value: 'talkpick' },
+      { label: '밸런스 게임', value: 'balance' },
+    ],
+    selectedValue: 'talkpick',
+  },
+};
+
+export const All: Story = {
+  args: {
+    items: [
+      { label: '톡픽', value: 'talkpick' },
+      { label: '밸런스 게임', value: 'balance' },
+    ],
+    selectedValue: 'talkpick',
+  },
+  render: (args) => {
+    const [selectedValue, setSelectedValue] = useState(args.selectedValue);
+
+    return (
+      <ul css={storyContainer}>
+        <li css={storyInnerContainer}>
+          <h3>토글 테스트</h3>
+          <SelectGroup
+            {...args}
+            selectedValue={selectedValue}
+            onSelect={setSelectedValue}
+          />
+        </li>
+        <li css={storyInnerContainer}>
+          <h3>톡픽 선택됨</h3>
+          <SelectGroup {...args} selectedValue="talkpick" />
+          <h3> 밸런스 게임 선택됨</h3>
+          <SelectGroup {...args} selectedValue="balance" />
+        </li>
+      </ul>
+    );
+  },
+};
