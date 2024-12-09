0205bf14d5b8919477306a359e9aa80a50e0b54c
hsgh085 <hsyu0917@gmail.com>
2024-12-10 02:58:16 +0900

    style: App.tsx 내 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@
 import MyPage from '@/pages/MyPage/MyPage';
 import SearchGamePage from '@/pages/SearchResultsPage/SearchGamePage';
 import SearchTalkPickPage from '@/pages/SearchResultsPage/SearchTalkPickPage';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import 'dayjs/locale/ko';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ProtectedRoutes from './components/Routes/ProtectedRoutes';
@@ -28,44 +25,39 @@ const App: React.FC = () => {
   useTokenRefresh();
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-      <Routes>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+        <Route path={PATH.SIGN_UP} element={<SignUpPage />} />
+        <Route path={PATH.LOGIN} element={<LoginPage />} />
+        <Route path={PATH.CHANGE.PASSWORD} element={<ChangePasswordPage />} />
+        <Route path={PATH.TODAY_TALKPICK} element={<TalkPickPage />} />
+        <Route path={PATH.TALKPICK_PLACE} element={<TalkPickPlacePage />} />
+        <Route path={PATH.TALKPICK()} element={<TalkPickPage />} />
+        <Route path={PATH.BALANCEGAME()} element={<BalanceGamePage />} />
+      </Route>
+
+      <Route path={`/${PATH.RESULT}`} element={<LayoutNoFooter />}>
+        <Route index element={<SearchResultsPage />} />
+        <Route path={PATH.SEARCH.ALL} element={<SearchResultsPage />} />
+        <Route path={PATH.SEARCH.TALKPICK} element={<SearchTalkPickPage />} />
+        <Route path={PATH.SEARCH.GAME} element={<SearchGamePage />} />
+      </Route>
+
+      <Route element={<ProtectedRoutes isLoggedIn={isLoggedIn} />}>
+        <Route path={PATH.MYPAGE} element={<LayoutNoFooter />}>
+          <Route index element={<MyPage />} />
+        </Route>
         <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />} />
-          <Route path={PATH.SIGN_UP} element={<SignUpPage />} />
-          <Route path={PATH.LOGIN} element={<LoginPage />} />
-          <Route path={PATH.CHANGE.PASSWORD} element={<ChangePasswordPage />} />
-          <Route path={PATH.TODAY_TALKPICK} element={<TalkPickPage />} />
-          <Route path={PATH.TALKPICK_PLACE} element={<TalkPickPlacePage />} />
-          <Route path={PATH.TALKPICK()} element={<TalkPickPage />} />
-          <Route path={PATH.BALANCEGAME()} element={<BalanceGamePage />} />
+          <Route path={PATH.CREATE.TALK_PICK} element={<CreatePostPage />} />
+          <Route
+            path={PATH.CREATE.GAME}
+            element={<BalanceGameCreationPage />}
+          />
+          <Route path={PATH.CHANGE.PROFILE} element={<ChangeUserInfoPage />} />
         </Route>
-
-        <Route path={`/${PATH.RESULT}`} element={<LayoutNoFooter />}>
-          <Route index element={<SearchResultsPage />} />
-          <Route path={PATH.SEARCH.ALL} element={<SearchResultsPage />} />
-          <Route path={PATH.SEARCH.TALKPICK} element={<SearchTalkPickPage />} />
-          <Route path={PATH.SEARCH.GAME} element={<SearchGamePage />} />
-        </Route>
-
-        <Route element={<ProtectedRoutes isLoggedIn={isLoggedIn} />}>
-          <Route path={PATH.MYPAGE} element={<LayoutNoFooter />}>
-            <Route index element={<MyPage />} />
-          </Route>
-          <Route path="/" element={<Layout />}>
-            <Route path={PATH.CREATE.TALK_PICK} element={<CreatePostPage />} />
-            <Route
-              path={PATH.CREATE.GAME}
-              element={<BalanceGameCreationPage />}
-            />
-            <Route
-              path={PATH.CHANGE.PROFILE}
-              element={<ChangeUserInfoPage />}
-            />
-          </Route>
-        </Route>
-      </Routes>
-    </LocalizationProvider>
+      </Route>
+    </Routes>
   );
 };
 
