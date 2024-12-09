87a651921e13ba249b2c9a58d1bb6664ae5a504c
hsgh085 <hsyu0917@gmail.com>
2024-12-09 22:50:56 +0900

    style: interceptor 주석제거

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -13,8 +13,6 @@ const baseURL =
   process.env.NODE_ENV === 'production' ? process.env.API_URL : '/api';
 
 export const axiosInstance = axios.create({
-  // baseURL: process.env.API_URL,
-  // baseURL: '/api',
   baseURL,
   headers: {
     'Content-Type': 'application/json',
@@ -38,12 +36,9 @@ axiosInstance.interceptors.request.use(
       newConfig.headers.Authorization = `Bearer ${accessToken}`;
     }
 
-    // console.log('요청 전 config', newConfig);
     return newConfig;
   },
   (error: AxiosError<AxiosErrorResponse>) => {
-    // console.log('요청 전 config 에러');
-
     return Promise.reject(error);
   },
 );
@@ -51,33 +46,13 @@ axiosInstance.interceptors.request.use(
 // response interceptor (after request)
 axiosInstance.interceptors.response.use(
   (response) => {
-    // console.log('요청 후 response');
     return response;
   },
   (error: AxiosError<AxiosErrorResponse>) => {
-    // console.log('요청 후 response 에러');
     const originalRequest = error.config;
     if (!error.response || !originalRequest) throw error;
 
     const { data, status } = error.response;
-    // const refreshToken = localStorage.getItem('rtk');
-
-    // if (refreshToken) {
-    //   if (status === HTTP_STATUS_CODE.UNAUTHORIZED) {
-    //     const accessToken = getRefreshToken();
-    //     console.log('new accessToken: ', accessToken);
-    //     localStorage.setItem('accessToken', accessToken);
-    //     store.dispatch({ type: 'token/setAccessToken', payload: accessToken });
-    //     originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-    //     return axiosInstance(originalRequest);
-    //     console.log('토큰 재발급');
-    //   }
-    //   if (status === HTTP_STATUS_CODE.BAD_REQUEST) {
-    //     localStorage.removeItem('accessToken');
-    //     localStorage.removeItem('rtk');
-    //     window.location.href = '/';
-    //   }
-    // }
     throw new HTTPError(status, data.httpStatus, data.message);
   },
 );
