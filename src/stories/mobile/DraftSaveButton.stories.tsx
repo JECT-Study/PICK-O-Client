f9dfd3f6ad79542dd4047af2f1d5572a42561fb9
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-12-09 22:02:33 +0900

    [feat] : DraftSaveButton 컴포넌트 UI 및 스토리북 구현

---
+++ src/stories/mobile/DraftSaveButton.stories.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import DraftSaveButton from '@/components/mobile/atom/DraftSaveButton/DraftSaveButton';
+import { BrowserRouter } from 'react-router-dom';
+
+const meta: Meta<typeof DraftSaveButton> = {
+  title: 'mobile/DraftSaveButton',
+  component: DraftSaveButton,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    onClick: () => console.log('clicked'),
+  },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
