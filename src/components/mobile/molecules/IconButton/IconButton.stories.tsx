d7fec467b296bfa897c5f219c9c08201ad03c7c6
onejuneee <onejuneee721@gmail.com>
2024-12-09 14:53:49 +0900

    fix: 저장되어있는 태그를 이용해 스토리북 스타일링

--- src/components/mobile/molecules/IconButton/IconButton.stories.tsx
+++ src/components/mobile/molecules/IconButton/IconButton.stories.tsx
@@ -7,6 +7,7 @@ import {
   IcMobileCommentDF,
 } from '@/assets';
 import IconButton from '@/components/mobile/molecules/IconButton/IconButton';
+import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
 
 const meta: Meta<typeof IconButton> = {
   title: 'mobile/molecules/IconButton',
@@ -36,35 +37,37 @@ export const Default: Story = {
 
 export const All: Story = {
   render: (args) => (
-    <div style={{ display: 'flex', gap: '16px' }}>
-      <IconButton
-        {...args}
-        activeIcon={<IcMobileBookmark />}
-        inactiveIcon={<IcMobileBookmarkDF />}
-        label="내가 저장한"
-        isActive={false}
-      />
-      <IconButton
-        {...args}
-        activeIcon={<IcMobileBookmark />}
-        inactiveIcon={<IcMobileBookmarkDF />}
-        label="내가 저장한"
-        isActive
-      />
-      <IconButton
-        {...args}
-        activeIcon={<IcMobileComment />}
-        inactiveIcon={<IcMobileCommentDF />}
-        label="내가 댓글단"
-        isActive={false}
-      />
-      <IconButton
-        {...args}
-        activeIcon={<IcMobileComment />}
-        inactiveIcon={<IcMobileCommentDF />}
-        label="내가 댓글단"
-        isActive
-      />
-    </div>
+    <ul css={storyContainer}>
+      <li css={storyInnerContainer}>
+        <IconButton
+          {...args}
+          activeIcon={<IcMobileBookmark />}
+          inactiveIcon={<IcMobileBookmarkDF />}
+          label="내가 저장한"
+          isActive={false}
+        />
+        <IconButton
+          {...args}
+          activeIcon={<IcMobileBookmark />}
+          inactiveIcon={<IcMobileBookmarkDF />}
+          label="내가 저장한"
+          isActive
+        />
+        <IconButton
+          {...args}
+          activeIcon={<IcMobileComment />}
+          inactiveIcon={<IcMobileCommentDF />}
+          label="내가 댓글단"
+          isActive={false}
+        />
+        <IconButton
+          {...args}
+          activeIcon={<IcMobileComment />}
+          inactiveIcon={<IcMobileCommentDF />}
+          label="내가 댓글단"
+          isActive
+        />
+      </li>
+    </ul>
   ),
 };
