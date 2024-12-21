9459b6c19cedcdb97c91ea53485395d3a1527d20
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-12-21 22:59:03 +0900

    feat: todayTalkPick dummy data 임시 구현

---
+++ src/mocks/data/banner.ts
@@ -0,0 +1,16 @@
+import { TodayTalkPick } from '@/types/talk-pick';
+
+export const todayTalkPickDummyData: TodayTalkPick[] = [
+  {
+    id: 20000,
+    title: '제목 #20000',
+    optionA: 'A',
+    optionB: 'B',
+  },
+  {
+    id: 20000,
+    title: '제목 #20000',
+    optionA: 'A',
+    optionB: 'B',
+  },
+];
