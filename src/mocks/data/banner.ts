c88c0aba1233a3f007d81df1bd52c194fabfdb8d
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-12-23 16:53:56 +0900

    feat: TopBanner props에 대한 더미데이터 생성 및 적용

--- src/mocks/data/banner.ts
+++ src/mocks/data/banner.ts
@@ -1,3 +1,5 @@
+import { SampleFirst, SampleSecond } from '@/assets';
+import { GameContent } from '@/types/game';
 import { TodayTalkPick } from '@/types/talk-pick';
 
 export const todayTalkPickDummyData: TodayTalkPick[] = [
@@ -12,5 +14,24 @@ export const todayTalkPickDummyData: TodayTalkPick[] = [
     title: '제목 #20000',
     optionA: 'A',
     optionB: 'B',
+  },
+];
+
+export const todayBalanceGameDummyData: GameContent[] = [
+  {
+    id: 1,
+    title: '남자친구와 첫 데이트 어디가 좋을까?',
+    mainTag: '커플',
+    subTag: '연애',
+    images: [SampleFirst, SampleSecond],
+    bookmarkState: false,
+  },
+  {
+    id: 2,
+    title: '남자친구와 첫 데이트 어디가 좋을까?',
+    mainTag: '커플',
+    subTag: '연애',
+    images: [SampleFirst, SampleSecond],
+    bookmarkState: false,
   },
 ];
