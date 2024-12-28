a9f01919f8f702e28cb491b299eea75632b614d9
onejuneee <onejuneee721@gmail.com>
2024-12-29 08:05:03 +0900

    feat: 버튼의 활성화 효과를 molecules 컴포넌트에서 선언합니다.

--- src/components/mobile/molecules/IconButton/IconButton.style.ts
+++ src/components/mobile/molecules/IconButton/IconButton.style.ts
@@ -12,10 +12,22 @@ export const iconButtonStyle = css({
   cursor: 'pointer',
   color: color.GY[1],
   transition: 'background-color 0.3s ease, color 0.3s ease',
+  position: 'relative',
 });
 
 export const activeStyle = css({
   color: color.BK,
+
+  '&::after': {
+    content: "''",
+    position: 'absolute',
+    bottom: '-4px',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    width: '16px',
+    height: '2px',
+    backgroundColor: color.MAIN,
+  },
 });
 
 export const labelStyle = css(typo.Mobile.Text.SemiBold_12, {
