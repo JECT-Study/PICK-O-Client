06b2c01c84d1d4c842266a17bb9a4cf47e673621
onejuneee <onejuneee721@gmail.com>
2025-01-09 05:11:32 +0900

    refactor: css 선언 방식을 객체 스타일링으로 수정합니다.

--- src/pages/MyPage/MyPage.style.ts
+++ src/pages/MyPage/MyPage.style.ts
@@ -1,37 +1,38 @@
 import { css } from '@emotion/react';
 import color from '@/styles/color';
 
-export const pageContainer = css`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-  height: 100%;
-  padding: 60px 0 20px 0;
-  background-color: ${color.WT}
-  overflow: hidden;
-  margin-top: 100px;
-  justify-content: center;
-`;
+export const pageContainer = css({
+  display: 'flex',
+  flexDirection: 'row',
+  width: '100%',
+  height: '100%',
+  padding: '60px 0 20px 0',
+  backgroundColor: color.WT,
+  overflow: 'hidden',
+  marginTop: '100px',
+  justifyContent: 'center',
+});
 
-export const contentWrapper = css`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  max-width: 904px;
-  margin-left: 20px;
-`;
+export const contentWrapper = css({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  maxWidth: '904px',
+  marginLeft: '20px',
+});
 
-export const contentList = css`
-  margin-top: 15px;
-  background-color: ${color.WT};
-  overflow-y: auto;
-  position: relative;
-`;
-export const loader = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  color: ${color.BK};
-  margin-top: 10px;
-`;
+export const contentList = css({
+  marginTop: '15px',
+  backgroundColor: color.WT,
+  overflowY: 'auto',
+  position: 'relative',
+});
+
+export const loader = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '20px',
+  color: color.BK,
+  marginTop: '10px',
+});
