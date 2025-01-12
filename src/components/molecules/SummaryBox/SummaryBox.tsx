f25d2d625461c49da7da8fa4c04f7231effbc02b
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-01-12 19:58:45 +0900

    feat: SummaryBox props로 톡픽 요약 상태 추가 및 상태에 따른 UI 구현

--- src/components/molecules/SummaryBox/SummaryBox.tsx
+++ src/components/molecules/SummaryBox/SummaryBox.tsx
@@ -1,31 +1,63 @@
 import React from 'react';
 import { TalkPickSummary } from '@/types/talk-pick';
-import { Spinner } from '@/assets';
+import { SadEmoji, Spinner, StatusFail, StatusNotRequired } from '@/assets';
 import SummaryItem from '@/components/atoms/SummaryItem/SummaryItem';
 import * as S from './SummaryBox.style';
 
 export interface SummaryBoxProps {
   summary?: TalkPickSummary;
+  summaryStatus?: 'PENDING' | 'SUCCESS' | 'FAIL' | 'NOT_REQUIRED';
 }
 
-const SummaryBox = ({ summary }: SummaryBoxProps) => (
-  <div css={S.summaryBoxStyling}>
-    <div css={S.summaryTextStyling}>세 줄 요약</div>
-    {summary?.summaryThirdLine === null ? (
+const SummaryBox = ({ summary, summaryStatus }: SummaryBoxProps) => {
+  const contentMap: Record<
+    'PENDING' | 'SUCCESS' | 'FAIL' | 'NOT_REQUIRED',
+    React.ReactNode
+  > = {
+    PENDING: (
       <div css={S.summarySpinnerWrapper}>
         <div css={S.summarySpinnerStyling}>
           <Spinner />
         </div>
         <p css={S.summarySpinnerText}>AI가 세 줄 요약을 하고 있어요!</p>
       </div>
-    ) : (
+    ),
+    SUCCESS: (
       <div css={S.summaryWrapper}>
         <SummaryItem itemNumber="1">{summary?.summaryFirstLine}</SummaryItem>
         <SummaryItem itemNumber="2">{summary?.summarySecondLine}</SummaryItem>
         <SummaryItem itemNumber="3">{summary?.summaryThirdLine}</SummaryItem>
       </div>
-    )}
-  </div>
-);
+    ),
+    FAIL: (
+      <div css={S.summaryStatusWrapper}>
+        <StatusFail />
+        <div css={S.summaryTextWrapper}>
+          <p>알 수 없는 오류가 발생했어요.</p>
+          <p>새로고침을 눌러주세요!</p>
+        </div>
+      </div>
+    ),
+    NOT_REQUIRED: (
+      <div css={S.summaryStatusWrapper}>
+        <StatusNotRequired />
+        <div css={S.summaryTextWrapper}>
+          <span css={S.summaryText}>
+            본문이 너무 짧아 요약할 수 없어요. <SadEmoji />
+          </span>
+          <p>전체 글을 확인해 주세요!</p>
+        </div>
+      </div>
+    ),
+  };
 
+  const renderContent = contentMap[summaryStatus || 'PENDING'];
+
+  return (
+    <div css={S.summaryBoxStyling}>
+      <div css={S.summaryTextStyling}>세 줄 요약</div>
+      {renderContent}
+    </div>
+  );
+};
 export default SummaryBox;
