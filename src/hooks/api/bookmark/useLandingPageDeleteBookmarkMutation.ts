6ab51b27b71ba2af677d9b05ef50406a95aeb93b
onejuneee <onejuneee721@gmail.com>
2025-01-20 17:41:53 +0900

    feat: 랜딩페이지 전용 북마크 래퍼를 생성합니다.

---
+++ src/hooks/api/bookmark/useLandingPageDeleteBookmarkMutation.ts
@@ -0,0 +1,68 @@
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { deleteDoneGameBookmark } from '@/api/bookmarks';
+import { Id, ServerResponse } from '@/types/api';
+import { BookmarkContext } from '@/types/bookmarks';
+import { GameContent } from '@/types/game';
+
+export const useLandingPageDeleteBookmarkMutation = (activeTab: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<AxiosResponse<ServerResponse>, Error, Id, BookmarkContext>(
+    {
+      mutationFn: (gameId: Id) => deleteDoneGameBookmark(gameId),
+
+      onMutate: (gameId: Id): BookmarkContext => {
+        const prevBest = queryClient.getQueryData<GameContent[]>([
+          'bestGames',
+          activeTab,
+        ]);
+        const prevLatest = queryClient.getQueryData<GameContent[]>([
+          'latestGames',
+          activeTab,
+        ]);
+
+        if (prevBest) {
+          const updatedBest = prevBest.map((item) =>
+            item.id === gameId ? { ...item, bookmarked: false } : item,
+          );
+          queryClient.setQueryData(['bestGames', activeTab], updatedBest);
+        }
+        if (prevLatest) {
+          const updatedLatest = prevLatest.map((item) =>
+            item.id === gameId ? { ...item, bookmarked: false } : item,
+          );
+          queryClient.setQueryData(['latestGames', activeTab], updatedLatest);
+        }
+        return { bestGames: prevBest, latestGames: prevLatest };
+      },
+
+      onError: (error, gameId, context) => {
+        if (context?.bestGames) {
+          queryClient.setQueryData(['bestGames', activeTab], context.bestGames);
+        }
+        if (context?.latestGames) {
+          queryClient.setQueryData(
+            ['latestGames', activeTab],
+            context.latestGames,
+          );
+        }
+        alert('북마크 해제 실패');
+      },
+
+      onSuccess: async (data, gameId, context) => {
+        try {
+          await queryClient.invalidateQueries({
+            queryKey: ['bestGames', activeTab],
+          });
+          await queryClient.invalidateQueries({
+            queryKey: ['latestGames', activeTab],
+          });
+        } catch (error) {
+          console.error('Failed to invalidate queries:', error);
+        }
+        alert('북마크 해제 완료!');
+      },
+    },
+  );
+};
