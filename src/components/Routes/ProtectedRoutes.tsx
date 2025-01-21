1b7a916281cce1eb2f3f7daf48359478ba6d3e7e
areumH <94692566+areumH@users.noreply.github.com>
2025-01-22 03:01:44 +0900

    refactor: ProtectedRoutes의 불필요한 토큰 재발급 함수 제거

--- src/components/Routes/ProtectedRoutes.tsx
+++ src/components/Routes/ProtectedRoutes.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { PATH } from '@/constants/path';
-import { useTokenRefresh } from '@/hooks/common/useTokenRefresh';
 import { Navigate, Outlet } from 'react-router-dom';
 
 type Props = {
@@ -8,8 +7,6 @@ type Props = {
 };
 
 const ProtectedRoutes = ({ isLoggedIn }: Props) => {
-  useTokenRefresh();
-
   return isLoggedIn ? <Outlet /> : <Navigate to={PATH.LOGIN} />;
 };
 
