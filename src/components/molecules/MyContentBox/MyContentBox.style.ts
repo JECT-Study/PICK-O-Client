209dae3e6981c7ed10c87d0985428254b0be584f
onejuneee <onejuneee721@gmail.com>
2025-01-24 06:32:07 +0900

    refactor: 스타일링 방식을 객체 리터럴 방식으로 수정하고 cursor 속성을 추가합니다.

--- src/components/molecules/MyContentBox/MyContentBox.style.ts
+++ src/components/molecules/MyContentBox/MyContentBox.style.ts
@@ -2,46 +2,47 @@ import { css } from '@emotion/react';
 import color from '@/styles/color';
 import typo from '@/styles/typo';
 
-export const infoContainer = css`
-  all: unset;
-  display: flex;
-  width: 904px;
-  height: 61px;
-  padding: 15px 40px;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  border: 1.5px solid ${color.GY[2]};
-  border-radius: 15px;
-  background-color: ${color.WT};
-  box-sizing: border-box;
+export const infoContainer = css({
+  all: 'unset',
+  display: 'flex',
+  width: '904px',
+  height: '61px',
+  padding: '15px 40px',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '10px',
+  border: `1.5px solid ${color.GY[2]}`,
+  borderRadius: '15px',
+  backgroundColor: color.WT,
+  boxSizing: 'border-box',
+  cursor: 'pointer',
 
-  &:focus-visible {
-    outline: 1px solid ${color.BK};
-    outline-offset: 1px;
-  }
-`;
+  '&:focus-visible': {
+    outline: `1px solid ${color.BK}`,
+    outlineOffset: '1px',
+  },
+});
 
-export const textContainer = css`
-  display: flex;
-  width: 546px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  gap: -2px;
-  align-self: stretch;
-  margin-right: auto;
-`;
+export const textContainer = css({
+  display: 'flex',
+  width: '546px',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  gap: '-2px',
+  alignSelf: 'stretch',
+  marginRight: 'auto',
+});
 
-export const titleLabel = css`
-  width: 100%;
-  ${typo.Main.Medium_16};
-  color: ${color.BK};
-  margin: 0;
-  text-align: left;
-`;
+export const titleLabel = css({
+  width: '100%',
+  ...typo.Main.Medium_16,
+  color: color.BK,
+  margin: 0,
+  textAlign: 'left',
+});
 
-export const bookmarkWrapper = css`
-  margin-left: auto;
-`;
+export const bookmarkWrapper = css({
+  marginLeft: 'auto',
+});
