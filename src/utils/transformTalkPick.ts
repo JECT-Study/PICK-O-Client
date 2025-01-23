ee0c05e93875a4ce3c5a5bc9f144363f04e1dee2
onejuneee <onejuneee721@gmail.com>
2025-01-24 04:02:59 +0900

    feat: 톡픽 관련 데이터 변환 유틸리티 함수를 추가합니다.

--- src/utils/transformTalkPick.ts
+++ src/utils/transformTalkPick.ts
@@ -1,6 +1,9 @@
 import {
   BookmarkInfoItemResponse,
+  CommentInfoItemResponse,
+  InfoItem,
   MyContentItem,
+  VoteInfoItemResponse,
   WrittenInfoItemResponse,
 } from '@/types/mypages';
 
@@ -32,16 +35,28 @@ export const transformWrittenItem = (
   };
 };
 
-// export const transformVoteItem = (
-//   item: VoteInfoItemResponse,
-// ): MyContentItem => {
-//   return {
-//     id: item.id,
-//     title: item.title,
-//     bookmarks: item.bookmarks,
-//     commentCount: item.commentCount,
-//     editedAt: item.editedAt,
-//     bookmarked: item.bookmarked,
-//     showBookmark: false,
-//   };
-// };
+export const transformVoteItem = (item: VoteInfoItemResponse): InfoItem => {
+  return {
+    id: item.id,
+    editedAt: item.editedAt,
+    title: item.title,
+    prefix: '내 선택',
+    content: item.voteOption,
+    commentCount: item.commentCount,
+    bookmarks: item.bookmarks,
+  };
+};
+
+export const transformCommentItem = (
+  item: CommentInfoItemResponse,
+): InfoItem => {
+  return {
+    id: item.id,
+    editedAt: item.editedAt,
+    title: item.title,
+    prefix: '내 댓글',
+    content: item.commentContent,
+    commentCount: item.commentCount,
+    bookmarks: item.bookmarks,
+  };
+};
