7b6b1ec9dd945bb17618900894b6a96c6178c702
onejuneee <onejuneee721@gmail.com>
2025-01-31 01:55:04 +0900

    refactor: 밸런스게임용 공통 북마크 훅이 전역 상태를 참조하도록 수정합니다.

--- src/hooks/mypages/useBalanceGameBookmark.ts
+++ src/hooks/mypages/useBalanceGameBookmark.ts
@@ -1,11 +1,13 @@
 import { useMyBalanceGameBookmarkCreateMutation } from '@/hooks/api/bookmark/useMyBalanceGameBookmarkCreateMutation';
 import { useMyBalanceGameBookmarkDeleteMutation } from '@/hooks/api/bookmark/useMyBalanceGameBookmarkDeleteMutation';
-import useToastModal from '@/hooks/modal/useToastModal';
 import { ERROR, SUCCESS } from '@/constants/message';
 import { MyBalanceGameItem } from '@/types/mypages';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '@/store';
+import { showToast } from '@/store/slice/toastSlice';
 
 export const useBalanceGameBookmark = () => {
-  const { showToastModal } = useToastModal();
+  const dispatch = useDispatch<AppDispatch>();
 
   const createBookmark = useMyBalanceGameBookmarkCreateMutation();
   const deleteBookmark = useMyBalanceGameBookmarkDeleteMutation();
@@ -15,13 +17,21 @@ export const useBalanceGameBookmark = () => {
 
     if (bookmarked) {
       deleteBookmark.mutate(gameSetId, {
-        onSuccess: () => showToastModal(SUCCESS.BOOKMARK.DELETE_MUTATE_SUCCESS),
-        onError: () => showToastModal(ERROR.BOOKMARK.DELETE_MUTATE_FAIL),
+        onSuccess: () =>
+          dispatch(
+            showToast({ message: SUCCESS.BOOKMARK.DELETE_MUTATE_SUCCESS }),
+          ),
+        onError: () =>
+          dispatch(showToast({ message: ERROR.BOOKMARK.DELETE_MUTATE_FAIL })),
       });
     } else {
       createBookmark.mutate(gameSetId, {
-        onSuccess: () => showToastModal(SUCCESS.BOOKMARK.POST_MUTATE_SUCCESS),
-        onError: () => showToastModal(ERROR.BOOKMARK.POST_MUTATE_FAIL),
+        onSuccess: () =>
+          dispatch(
+            showToast({ message: SUCCESS.BOOKMARK.POST_MUTATE_SUCCESS }),
+          ),
+        onError: () =>
+          dispatch(showToast({ message: ERROR.BOOKMARK.POST_MUTATE_FAIL })),
       });
     }
   };
