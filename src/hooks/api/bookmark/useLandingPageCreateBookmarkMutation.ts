fdfa62eab0c3cf8964e8a56e055c174798d03f58
onejuneee <onejuneee721@gmail.com>
2025-01-31 21:32:20 +0900

    refactor: 랜딩 페이지 북마크 훅에 대한 래퍼 훅을 사용합니다.

--- src/hooks/api/bookmark/useLandingPageCreateBookmarkMutation.ts
+++ src/hooks/api/bookmark/useLandingPageCreateBookmarkMutation.ts
@@ -1,62 +1,6 @@
-import { useQueryClient, useMutation } from '@tanstack/react-query';
-import { AxiosResponse } from 'axios';
 import { postDoneGameBookmark } from '@/api/bookmarks';
-import { Id, ServerResponse } from '@/types/api';
-import { BookmarkContext } from '@/types/bookmarks';
-import { GameContent } from '@/types/game';
+import { useLandingPageBookmarkMutation } from './useLandingPageBookmarkMutation';
 
 export const useLandingPageCreateBookmarkMutation = (activeTab: string) => {
-  const queryClient = useQueryClient();
-
-  return useMutation<AxiosResponse<ServerResponse>, Error, Id, BookmarkContext>(
-    {
-      mutationFn: (gameId: Id) => postDoneGameBookmark(gameId),
-
-      onMutate: (gameId: Id): BookmarkContext => {
-        const prevBest = queryClient.getQueryData<GameContent[]>([
-          'bestGames',
-          activeTab,
-        ]);
-        const prevLatest = queryClient.getQueryData<GameContent[]>([
-          'latestGames',
-          activeTab,
-        ]);
-
-        if (prevBest) {
-          const updatedBest = prevBest.map((item) =>
-            item.id === gameId ? { ...item, bookmarked: true } : item,
-          );
-          queryClient.setQueryData(['bestGames', activeTab], updatedBest);
-        }
-        if (prevLatest) {
-          const updatedLatest = prevLatest.map((item) =>
-            item.id === gameId ? { ...item, bookmarked: true } : item,
-          );
-          queryClient.setQueryData(['latestGames', activeTab], updatedLatest);
-        }
-        return { bestGames: prevBest, latestGames: prevLatest };
-      },
-
-      onError: (error, gameId, context) => {
-        if (context?.bestGames) {
-          queryClient.setQueryData(['bestGames', activeTab], context.bestGames);
-        }
-        if (context?.latestGames) {
-          queryClient.setQueryData(
-            ['latestGames', activeTab],
-            context.latestGames,
-          );
-        }
-      },
-
-      onSuccess: async () => {
-        await queryClient.invalidateQueries({
-          queryKey: ['bestGames', activeTab],
-        });
-        await queryClient.invalidateQueries({
-          queryKey: ['latestGames', activeTab],
-        });
-      },
-    },
-  );
+  return useLandingPageBookmarkMutation(activeTab, postDoneGameBookmark, true);
 };
--- src/hooks/api/bookmark/useLandingPageDeleteBookmarkMutation.ts
+++ src/hooks/api/bookmark/useLandingPageDeleteBookmarkMutation.ts
@@ -1,62 +1,10 @@
-import { useQueryClient, useMutation } from '@tanstack/react-query';
-import { AxiosResponse } from 'axios';
 import { deleteDoneGameBookmark } from '@/api/bookmarks';
-import { Id, ServerResponse } from '@/types/api';
-import { BookmarkContext } from '@/types/bookmarks';
-import { GameContent } from '@/types/game';
+import { useLandingPageBookmarkMutation } from './useLandingPageBookmarkMutation';
 
 export const useLandingPageDeleteBookmarkMutation = (activeTab: string) => {
-  const queryClient = useQueryClient();
-
-  return useMutation<AxiosResponse<ServerResponse>, Error, Id, BookmarkContext>(
-    {
-      mutationFn: (gameId: Id) => deleteDoneGameBookmark(gameId),
-
-      onMutate: (gameId: Id): BookmarkContext => {
-        const prevBest = queryClient.getQueryData<GameContent[]>([
-          'bestGames',
-          activeTab,
-        ]);
-        const prevLatest = queryClient.getQueryData<GameContent[]>([
-          'latestGames',
-          activeTab,
-        ]);
-
-        if (prevBest) {
-          const updatedBest = prevBest.map((item) =>
-            item.id === gameId ? { ...item, bookmarked: false } : item,
-          );
-          queryClient.setQueryData(['bestGames', activeTab], updatedBest);
-        }
-        if (prevLatest) {
-          const updatedLatest = prevLatest.map((item) =>
-            item.id === gameId ? { ...item, bookmarked: false } : item,
-          );
-          queryClient.setQueryData(['latestGames', activeTab], updatedLatest);
-        }
-        return { bestGames: prevBest, latestGames: prevLatest };
-      },
-
-      onError: (error, gameId, context) => {
-        if (context?.bestGames) {
-          queryClient.setQueryData(['bestGames', activeTab], context.bestGames);
-        }
-        if (context?.latestGames) {
-          queryClient.setQueryData(
-            ['latestGames', activeTab],
-            context.latestGames,
-          );
-        }
-      },
-
-      onSuccess: async () => {
-        await queryClient.invalidateQueries({
-          queryKey: ['bestGames', activeTab],
-        });
-        await queryClient.invalidateQueries({
-          queryKey: ['latestGames', activeTab],
-        });
-      },
-    },
+  return useLandingPageBookmarkMutation(
+    activeTab,
+    deleteDoneGameBookmark,
+    false,
   );
 };
