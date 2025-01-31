539ec47d92a1bbba4b9fa0dbab9926218eed20e6
onejuneee <onejuneee721@gmail.com>
2025-02-01 00:16:20 +0900

    refactor: 마이페이지 톡픽 북마크를 다루는 기존 훅들을 래퍼 훅으로 수정합니다.

--- src/hooks/api/bookmark/useMyTalkPickBookmarkCreateMutation.ts
+++ src/hooks/api/bookmark/useMyTalkPickBookmarkCreateMutation.ts
@@ -1,53 +1,6 @@
-import {
-  useQueryClient,
-  useMutation,
-  InfiniteData,
-} from '@tanstack/react-query';
-import { MyBookmarkTransformedPage } from '@/hooks/api/mypages/useMyTalkPickBookmarksQuery';
 import { postTalkPickBookmark } from '@/api/bookmarks';
-import { ServerResponse } from '@/types/api';
-import { AxiosResponse } from 'axios';
+import { useMyTalkPickBookmarkMutation } from '@/hooks/api/bookmark/useMyTalkPickBookmarkMutation';
 
 export const useMyTalkPickBookmarkCreateMutation = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation<
-    AxiosResponse<ServerResponse>,
-    Error,
-    number,
-    InfiniteData<MyBookmarkTransformedPage> | undefined
-  >({
-    mutationFn: (talkPickId: number) => postTalkPickBookmark(talkPickId),
-
-    onMutate: async (talkPickId) => {
-      await queryClient.cancelQueries({ queryKey: ['myBookmarks'] });
-
-      const previousData = queryClient.getQueryData<
-        InfiniteData<MyBookmarkTransformedPage>
-      >(['myBookmarks']);
-      if (previousData) {
-        const newPages = previousData.pages.map((page) => ({
-          ...page,
-          content: page.content.map((item) =>
-            item.id === talkPickId ? { ...item, bookmarked: true } : item,
-          ),
-        }));
-        queryClient.setQueryData(['myBookmarks'], {
-          ...previousData,
-          pages: newPages,
-        });
-      }
-      return previousData;
-    },
-
-    onError: (err, talkPickId, context) => {
-      if (context) {
-        queryClient.setQueryData(['myBookmarks'], context);
-      }
-    },
-
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ['myBookmarks'] });
-    },
-  });
+  return useMyTalkPickBookmarkMutation(postTalkPickBookmark, true, true);
 };
--- src/hooks/api/bookmark/useMyTalkPickBookmarkDeleteMutation.ts
+++ src/hooks/api/bookmark/useMyTalkPickBookmarkDeleteMutation.ts
@@ -1,53 +1,6 @@
-import {
-  InfiniteData,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query';
 import { deleteTalkPickBookmark } from '@/api/bookmarks';
-import { MyBookmarkTransformedPage } from '@/hooks/api/mypages/useMyTalkPickBookmarksQuery';
-import { AxiosResponse } from 'axios';
-import { ServerResponse } from '@/types/api';
+import { useMyTalkPickBookmarkMutation } from '@/hooks/api/bookmark/useMyTalkPickBookmarkMutation';
 
 export const useMyTalkPickBookmarkDeleteMutation = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation<
-    AxiosResponse<ServerResponse>,
-    Error,
-    number,
-    InfiniteData<MyBookmarkTransformedPage> | undefined
-  >({
-    mutationFn: (talkPickId: number) => deleteTalkPickBookmark(talkPickId),
-
-    onMutate: async (talkPickId) => {
-      await queryClient.cancelQueries({ queryKey: ['myBookmarks'] });
-
-      const previousData = queryClient.getQueryData<
-        InfiniteData<MyBookmarkTransformedPage>
-      >(['myBookmarks']);
-
-      if (previousData) {
-        const newPages = previousData.pages.map((page) => ({
-          ...page,
-          content: page.content.map((item) =>
-            item.id === talkPickId ? { ...item, bookmarked: false } : item,
-          ),
-        }));
-
-        queryClient.setQueryData(['myBookmarks'], {
-          ...previousData,
-          pages: newPages,
-        });
-      }
-      return previousData;
-    },
-
-    onError: (err, talkPickId, context) => {
-      if (context) {
-        queryClient.setQueryData(['myBookmarks'], context);
-      }
-    },
-
-    onSuccess: async () => {},
-  });
+  return useMyTalkPickBookmarkMutation(deleteTalkPickBookmark, false, false);
 };
