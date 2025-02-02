0cf3f5f1da7cc5bf2e69a80981c0aa0de3ad7ffb
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-02-02 20:56:34 +0900

    style: 더보기 버튼에 대한 모바일 스타일 추가

--- src/components/atoms/MoreButton/MoreButton.style.ts
+++ src/components/atoms/MoreButton/MoreButton.style.ts
@@ -4,10 +4,14 @@ import typo from '@/styles/typo';
 import type { MoreButtonProps } from './MoreButton';
 
 export const moreButtonStyling = (icon: Required<MoreButtonProps>['icon']) =>
-  css(typo.Main.Medium, {
+  css({
+    ...typo.Main.Medium,
     display: 'flex',
     alignItems: 'center',
     gap: '7px',
     color: icon === 'plus' ? color.GY[1] : color.MAIN,
     cursor: 'pointer',
+    '@media (max-width: 430px)': {
+      ...typo.Mobile.Text.SemiBold_14,
+    },
   });
