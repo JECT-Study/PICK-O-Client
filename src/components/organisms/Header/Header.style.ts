c6e76b07c7c2bf5039305f84388a1d8458f80c07
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-02-03 14:45:07 +0900

    refactor: 헤더 css 공통 스타일로 일치 및 모바일 헤더 스타일 수정

--- src/components/organisms/Header/Header.style.ts
+++ src/components/organisms/Header/Header.style.ts
@@ -2,58 +2,57 @@ import { css } from '@emotion/react';
 import color from '@/styles/color';
 import typo from '@/styles/typo';
 
-export const containerStyle = css`
-  width: 100%;
-  padding: 0 200px;
-  height: 100px;
-  @media (max-width: 430px) {
-    padding: 0 20px;
-    height: 55px;
-  }
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: ${color.WT};
-  box-shadow: 0px 4px 11px rgba(0, 0, 0, 0.1);
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1000;
-`;
+export const containerStyle = css({
+  width: '100%',
+  padding: '0 200px',
+  height: '100px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  backgroundColor: color.WT,
+  boxShadow: '0px 4px 11px rgba(0, 0, 0, 0.1)',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  zIndex: 1000,
+  '@media (max-width: 430px)': {
+    padding: '0 20px',
+    height: '55px',
+  },
+});
 
-export const logoStyle = css`
-  display: flex;
-  align-items: center;
-  @media (max-width: 430px) {
-    flex: 1;
-    justify-content: center;
-  }
-`;
+export const logoStyle = css({
+  display: 'flex',
+  alignItems: 'center',
+  '@media (max-width: 430px)': {
+    flex: 1,
+  },
+});
 
-export const LoginButtonStyle = css`
-  ${typo.Main.SemiBold};
-  background: none;
-  border: none;
-  cursor: pointer;
-  color: ${color.GY[1]};
-  width: 100.24px;
-  height: 32.92px;
-  margin-right: 7.76px;
-`;
+export const LoginButtonStyle = css({
+  ...typo.Main.SemiBold,
+  background: 'none',
+  border: 'none',
+  cursor: 'pointer',
+  color: color.GY[1],
+  width: '100.24px',
+  height: '32.92px',
+  marginRight: '7.76px',
+});
 
-export const rightContainerStyle = css`
-  display: flex;
-  align-items: center;
-  @media screen and (max-width: 430px) {
-    flex: 1;
-    justify-content: flex-end;
-  }
-`;
+export const rightContainerStyle = css({
+  display: 'flex',
+  alignItems: 'center',
+  '@media (max-width: 430px)': {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+});
 
-export const notificationStyle = css`
-  margin-left: 42.93px;
-`;
+export const notificationStyle = css({
+  marginLeft: '42.93px',
+});
 
-export const listButtonStyle = css`
-  cursor: pointer;
-`;
+export const listButtonStyle = css({
+  cursor: 'pointer',
+});
