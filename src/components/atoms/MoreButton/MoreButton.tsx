90b03083e83598389dcbeff7d8b4463d06942b67
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-02-03 14:44:38 +0900

    refactor: MoreButton에 모바일 대응 아이콘 로직 추가

--- src/components/atoms/MoreButton/MoreButton.tsx
+++ src/components/atoms/MoreButton/MoreButton.tsx
@@ -1,19 +1,25 @@
 import React, { forwardRef, ComponentPropsWithRef, ForwardedRef } from 'react';
-import { More, MoreReply } from '@/assets';
+import { More, MoreReply, MoreSmall } from '@/assets';
 import * as S from './MoreButton.style';
 
 export interface MoreButtonProps extends ComponentPropsWithRef<'button'> {
   icon?: 'plus' | 'arrow';
+  size?: 'small' | 'default';
 }
 
 const MoreButton = (
-  { icon = 'plus', ...props }: MoreButtonProps,
+  { icon = 'plus', size = 'default', ...props }: MoreButtonProps,
   ref: ForwardedRef<HTMLButtonElement>,
 ) => {
+  const getIcon = () => {
+    if (size === 'small') return <MoreSmall />;
+    return icon === 'plus' ? <More /> : <MoreReply />;
+  };
+
   return (
     <button type="button" ref={ref} css={S.moreButtonStyling(icon)} {...props}>
       더보기
-      {icon === 'plus' ? <More /> : <MoreReply />}
+      {getIcon()}
     </button>
   );
 };
