0e6efe4a9e7bebb0445543477fc2403fb4a9f888
areumH <94692566+areumH@users.noreply.github.com>
2025-02-17 01:21:36 +0900

    feat: VoteToggle 컴포넌트 구현 및 스토리북 작성

---
+++ src/components/mobile/molecules/VoteToggle/VoteToggle.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { NOTICE } from '@/constants/message';
+import { PATH } from '@/constants/path';
+import { VoteOption, MyVoteOption } from '@/types/vote';
+import Button from '@/components/mobile/atoms/Button/Button';
+import ToastModal from '@/components/atoms/ToastModal/ToastModal';
+import { useNewSelector } from '@/store';
+import { selectAccessToken } from '@/store/auth';
+import { useCreateTalkPickVoteMutation } from '@/hooks/api/vote/useCreateTalkPickVoteMutation';
+import { useEditTalkPickVoteMutation } from '@/hooks/api/vote/useEditTalkPickVoteMutation';
+import { useDeleteTalkPickVoteMutation } from '@/hooks/api/vote/useDeleteTalkPickVoteMutation';
+import useToastModal from '@/hooks/modal/useToastModal';
+import * as S from './VoteToggle.style';
+
+interface VoteToggleProps {
+  talkPickId: number;
+  leftButtonText: string;
+  rightButtonText: string;
+  selectedVote: MyVoteOption;
+}
+
+const VoteToggle: React.FC<VoteToggleProps> = ({
+  talkPickId,
+  leftButtonText,
+  rightButtonText,
+  selectedVote,
+}) => {
+  const navigate = useNavigate();
+  const accessToken = useNewSelector(selectAccessToken);
+  const [loggedOutVoteOption, setLoggedOutVoteOption] =
+    useState<MyVoteOption>(null);
+
+  const currnetOption: MyVoteOption = accessToken
+    ? selectedVote
+    : loggedOutVoteOption;
+
+  const { isVisible, modalText, showToastModal } = useToastModal();
+
+  const { mutate: createTalkPickVote } =
+    useCreateTalkPickVoteMutation(talkPickId);
+
+  const { mutate: editTalkPickVote } = useEditTalkPickVoteMutation(talkPickId);
+
+  const { mutate: deleteTalkPickVote } =
+    useDeleteTalkPickVoteMutation(talkPickId);
+
+  useEffect(() => {
+    if (!accessToken) {
+      const savedVote = localStorage.getItem(`talkpick_${talkPickId}`);
+
+      if (savedVote === 'A' || savedVote === 'B') {
+        setLoggedOutVoteOption(savedVote);
+      }
+    }
+  }, [accessToken, talkPickId]);
+
+  const handleLoggedOutTalkPickVote = (voteOption: VoteOption) => {
+    if (loggedOutVoteOption === voteOption) {
+      setLoggedOutVoteOption(null);
+      localStorage.removeItem(`talkpick_${talkPickId}`);
+    } else {
+      setLoggedOutVoteOption(voteOption);
+      localStorage.setItem(`talkpick_${talkPickId}`, voteOption);
+
+      showToastModal(NOTICE.REQUIRED.LOGIN, () => {
+        navigate(`/${PATH.LOGIN}`, { state: { talkPickId } });
+      });
+    }
+  };
+
+  const handleTalkPickVote = (
+    selectedOption: MyVoteOption,
+    voteOption: VoteOption,
+  ) => {
+    if (selectedOption === null) {
+      createTalkPickVote(voteOption);
+    } else if (selectedOption === voteOption) {
+      deleteTalkPickVote();
+    } else {
+      editTalkPickVote(voteOption);
+    }
+  };
+
+  const handleVoteButtonClick = (voteOption: VoteOption) => {
+    if (accessToken) {
+      handleTalkPickVote(selectedVote, voteOption);
+    } else {
+      handleLoggedOutTalkPickVote(voteOption);
+    }
+  };
+
+  return (
+    <div css={S.voteToggleStyle}>
+      {isVisible && (
+        <div css={S.toastModalStyling}>
+          <ToastModal>{modalText}</ToastModal>
+        </div>
+      )}
+      <div css={S.buttonContainerStyle}>
+        <Button
+          variant="outlineHighlightR"
+          onClick={() => {
+            handleVoteButtonClick('A');
+          }}
+          css={S.getButtonStyle('A', currnetOption)}
+        >
+          {leftButtonText}
+        </Button>
+        <div css={S.voteTextStyle}>VS</div>
+        <Button
+          variant="outlineHighlightB"
+          onClick={() => {
+            handleVoteButtonClick('B');
+          }}
+          css={S.getButtonStyle('B', currnetOption)}
+        >
+          {rightButtonText}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default VoteToggle;
