21f2b435c0afd71aefb6a8a3248c467ee28a2dc5
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-02-17 19:35:33 +0900

    feat: 댓글 없는 상황에 대한 상수 메세지 정의

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -217,3 +217,8 @@ export const PROFILE = {
     SIZE_LIMIT: '3MB 이하의 사진만 가능합니다.',
   },
 };
+
+export const COMMENT = {
+  NO_COMMENTS_YET: '아직 댓글이 없어요',
+  FIRST_COMMENT_SUGGESTION: '첫 댓글을 달아보는건 어때요?',
+};
