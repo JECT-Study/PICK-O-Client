878f419c0558eea557c96cd535e41ea951d2b28b
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2025-03-06 01:40:46 +0900

    refactor: MobileReport 아이콘 정의 중복 제거

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -158,7 +158,6 @@ export { default as MobileLikeButtonPR } from './svg/mobile-like-button-pr.svg';
 export { default as MobileArrowUp } from './svg/mobile-arrow-up.svg';
 export { default as MobileArrowDown } from './svg/mobile-arrow-down.svg';
 export { default as MobileReport } from './svg/mobile-report.svg';
-export { default as MobileReport } from './svg/mobile-report.svg';
 export { default as PickIcon } from './svg/pick-icon.svg';
 
 // TODO: 이전 SVG
