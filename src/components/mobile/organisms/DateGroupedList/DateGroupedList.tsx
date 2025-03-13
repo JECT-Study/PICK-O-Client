72953d889abaeec1b50323dc6faad321a9068621
onejuneee <onejuneee721@gmail.com>
2025-03-14 07:23:29 +0900

    refactor: 하위 컴포넌트의 태그 변경 사항에 맞춰 이벤트 핸들러를 제거합니다.

--- src/components/mobile/organisms/DateGroupedList/DateGroupedList.tsx
+++ src/components/mobile/organisms/DateGroupedList/DateGroupedList.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ProfileListItem from '@/components/mobile/molecules/ProfileListItem/ProfileListItem';
-import { useNavigate } from 'react-router-dom';
 import * as S from './DateGroupedList.style';
 
 export interface DateGroupedListItem {
   id: number;
   title: string;
-  imgUrl: string;
+  imgUrl?: string;
 }
 
 export interface DateGroupedListProps {
@@ -14,29 +13,21 @@ export interface DateGroupedListProps {
   items: DateGroupedListItem[];
 }
 
-const DateGroupedList = ({ date, items }: DateGroupedListProps) => {
-  const navigate = useNavigate();
-
-  const handleItemClick = (id: number) => {
-    navigate(`/talkpick/${id}`);
-  };
-
-  return (
-    <div css={S.containerStyle}>
-      <span css={S.dateStyle}>{date}</span>
-      <ul css={S.listStyle}>
-        {items.map(({ id, title, imgUrl }) => (
-          <li
-            key={id}
-            css={S.listItemStyle}
-            onClick={() => handleItemClick(id)}
-          >
-            <ProfileListItem title={title} imgUrl={imgUrl} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const DateGroupedList = ({ date, items }: DateGroupedListProps) => (
+  <div css={S.containerStyle}>
+    <span css={S.dateStyle}>{date}</span>
+    <ul css={S.listStyle}>
+      {items.map(({ id, title, imgUrl }) => (
+        <li key={id} css={S.listItemStyle}>
+          <ProfileListItem
+            title={title}
+            imgUrl={imgUrl}
+            to={`/talkpick/${id}`}
+          />
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default DateGroupedList;
