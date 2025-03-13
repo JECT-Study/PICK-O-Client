e8f1eeb7217b138d00e10bb21a28228cb076603c
onejuneee <onejuneee721@gmail.com>
2025-03-14 07:20:37 +0900

    feat: 모바일 톡픽 컴포넌트에 클릭 핸들러를 추가합니다.

--- src/components/mobile/organisms/DateGroupedList/DateGroupedList.tsx
+++ src/components/mobile/organisms/DateGroupedList/DateGroupedList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProfileListItem from '@/components/mobile/molecules/ProfileListItem/ProfileListItem';
+import { useNavigate } from 'react-router-dom';
 import * as S from './DateGroupedList.style';
 
 export interface DateGroupedListItem {
@@ -13,17 +14,29 @@ export interface DateGroupedListProps {
   items: DateGroupedListItem[];
 }
 
-const DateGroupedList = ({ date, items }: DateGroupedListProps) => (
-  <div css={S.containerStyle}>
-    <span css={S.dateStyle}>{date}</span>
-    <ul css={S.listStyle}>
-      {items.map(({ id, title, imgUrl }) => (
-        <li key={id} css={S.listItemStyle}>
-          <ProfileListItem title={title} imgUrl={imgUrl} />
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const DateGroupedList = ({ date, items }: DateGroupedListProps) => {
+  const navigate = useNavigate();
+
+  const handleItemClick = (id: number) => {
+    navigate(`/talkpick/${id}`);
+  };
+
+  return (
+    <div css={S.containerStyle}>
+      <span css={S.dateStyle}>{date}</span>
+      <ul css={S.listStyle}>
+        {items.map(({ id, title, imgUrl }) => (
+          <li
+            key={id}
+            css={S.listItemStyle}
+            onClick={() => handleItemClick(id)}
+          >
+            <ProfileListItem title={title} imgUrl={imgUrl} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default DateGroupedList;
