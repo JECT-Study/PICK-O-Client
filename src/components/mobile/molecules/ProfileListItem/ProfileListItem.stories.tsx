ef1789c03d4430ebb97bcbfb935aefd0ac03d45f
onejuneee <onejuneee721@gmail.com>
2025-03-14 07:23:10 +0900

    docs: 컴포넌트의 태그 변경 사항에 맞춰 스토리북 항목을 추가합니다.

--- src/components/mobile/molecules/ProfileListItem/ProfileListItem.stories.tsx
+++ src/components/mobile/molecules/ProfileListItem/ProfileListItem.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import ProfileListItem from '@/components/mobile/molecules/ProfileListItem/ProfileListItem';
 import { ProfileInfoSample } from '@/assets';
+import { MemoryRouter } from 'react-router-dom';
 import { storyContainer, storyInnerContainer } from '@/stories/story.styles';
 
 const meta: Meta<typeof ProfileListItem> = {
@@ -21,6 +22,11 @@ const meta: Meta<typeof ProfileListItem> = {
       description: '이미지 URL',
       defaultValue: ProfileInfoSample,
     },
+    to: {
+      control: 'text',
+      description: '이동할 경로',
+      defaultValue: '/talkpick/1',
+    },
   },
 };
 
@@ -31,22 +37,46 @@ export const Default: Story = {
   args: {
     title: '제목제목제목제목',
     imgUrl: ProfileInfoSample,
+    to: '/talkpick/1',
   },
+  render: (args) => (
+    <MemoryRouter>
+      <ProfileListItem {...args} />
+    </MemoryRouter>
+  ),
 };
 
 export const All: Story = {
   render: (args) => (
-    <ul css={storyContainer}>
-      <li css={storyInnerContainer}>
-        <ProfileListItem {...args} title="제목1" imgUrl={ProfileInfoSample} />
-        <ProfileListItem {...args} title="제목2" imgUrl={ProfileInfoSample} />
-        <ProfileListItem {...args} title="제목3" imgUrl={ProfileInfoSample} />
-        <ProfileListItem
-          {...args}
-          title="제목제목제목제목제목"
-          imgUrl={ProfileInfoSample}
-        />
-      </li>
-    </ul>
+    <MemoryRouter>
+      <ul css={storyContainer}>
+        <li css={storyInnerContainer}>
+          <ProfileListItem
+            {...args}
+            title="제목1"
+            imgUrl={ProfileInfoSample}
+            to="/talkpick/1"
+          />
+          <ProfileListItem
+            {...args}
+            title="제목2"
+            imgUrl={ProfileInfoSample}
+            to="/talkpick/2"
+          />
+          <ProfileListItem
+            {...args}
+            title="제목3"
+            imgUrl={ProfileInfoSample}
+            to="/talkpick/3"
+          />
+          <ProfileListItem
+            {...args}
+            title="제목제목제목제목제목"
+            imgUrl={ProfileInfoSample}
+            to="/talkpick/4"
+          />
+        </li>
+      </ul>
+    </MemoryRouter>
   ),
 };
