dd9906f67070cf99f92c23d92b0b95d2171a2bee
onejuneee <onejuneee721@gmail.com>
2025-03-14 07:22:29 +0900

    feat: imgUrl이 존재하지 않을 때 랜덤 이미지가 출력 기능을 추가합니다.

--- src/components/mobile/molecules/ProfileListItem/ProfileListItem.tsx
+++ src/components/mobile/molecules/ProfileListItem/ProfileListItem.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import MobileProfileImage from '@/components/mobile/atoms/MobileProfileImage/MobileProfileImage';
+import {
+  RandomBlackFrame,
+  RandomBlueFrame,
+  RandomGreenFrame,
+  RandomPinkFrame,
+  RandomPurpleFrame,
+  RandomTealFrame,
+} from '@/assets';
 import * as S from './ProfileListItem.style';
+
+const randomImages = [
+  RandomBlackFrame,
+  RandomBlueFrame,
+  RandomGreenFrame,
+  RandomPinkFrame,
+  RandomPurpleFrame,
+  RandomTealFrame,
+];
+
+const useRandomImage = () => {
+  return useMemo(() => {
+    const randomIndex = Math.floor(Math.random() * randomImages.length);
+    return randomImages[randomIndex];
+  }, []);
+};
 
 export interface ProfileListItemProps {
   title: string;
-  imgUrl: string;
+  imgUrl?: string;
 }
 
-const ProfileListItem = ({ title, imgUrl }: ProfileListItemProps) => (
-  <div css={S.containerStyle}>
-    <span css={S.titleStyle}>{title}</span>
-    <MobileProfileImage
-      imgUrl={imgUrl}
-      alt={`${title}님의 프로필 이미지`}
-      size="sm"
-    />
-  </div>
-);
+const ProfileListItem = ({ title, imgUrl }: ProfileListItemProps) => {
+  const randomImage = useRandomImage();
+  const displayImgUrl = imgUrl || randomImage;
+
+  return (
+    <div css={S.containerStyle}>
+      <span css={S.titleStyle}>{title}</span>
+      <MobileProfileImage
+        imgUrl={displayImgUrl}
+        alt={`${title}님의 프로필 이미지`}
+        size="sm"
+      />
+    </div>
+  );
+};
 
 export default ProfileListItem;
