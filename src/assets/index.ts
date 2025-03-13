5ffe6ff8413e7d90082886127349ab80998254de
onejuneee <onejuneee721@gmail.com>
2025-03-14 07:02:19 +0900

    feat: 스토리북에서 이미지를 테스트하기 위한 샘플 이미지를 추가합니다.

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -153,6 +153,8 @@ export { default as MobileDefaultPerson } from './svg/mobile-default-person.svg'
 export { default as MobilePlus } from './svg/mobile-plus.svg';
 export { default as MobileReport } from './svg/mobile-report.svg';
 export { default as PickIcon } from './svg/pick-icon.svg';
+export { default as MobileCardSampleFirst } from './images/my-card-sample-first.png';
+export { default as MobileCardSampleSecond } from './images/my-card-sample-second.png';
 
 // TODO: 이전 SVG
 export { default as Email } from './svg/email.svg';
