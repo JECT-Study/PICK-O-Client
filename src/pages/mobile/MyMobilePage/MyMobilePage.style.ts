17d0b7a3709efed450523ce9209392ff7686fe44
onejuneee <onejuneee721@gmail.com>
2025-03-14 07:10:08 +0900

    feat: 모바일 마이페이지 레이아웃을 위한 추가 스타일을 생성합니다.

--- src/pages/mobile/MyMobilePage/MyMobilePage.style.ts
+++ src/pages/mobile/MyMobilePage/MyMobilePage.style.ts
@@ -1,8 +1,28 @@
 import { css } from '@emotion/react';
+import color from '@/styles/color';
 
 export const pageStyle = css({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  padding: '10px 0',
+  background: color.WT,
+  padding: '18px 20px',
+  width: '100%',
+});
+
+export const selectGroupWrapper = css({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '18px 0 16px 0',
+  width: '100%',
+});
+
+export const contentWrapper = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '18px 0',
+  width: '100%',
 });
